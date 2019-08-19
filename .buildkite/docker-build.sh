77455485effdf196fb95901fce0013f733b41737
gustavderdrache <aford@forumone.com>
2019-08-19 16:29:54 -0400

    EPAD8-13: Final build

--- .buildkite/docker-build.sh
+++ .buildkite/docker-build.sh
@@ -2,13 +2,11 @@
 
 set -exuo pipefail
 
-eval "$(aws --debug ecr get-login --region=us-east-1 --no-include-email)"
-
 tag="$BUILDKITE_BRANCH-$BUILDKITE_BUILD_NUMBER"
 image_name="$DOCKER_REPOSITORY:$tag"
 
 docker build -t "$image_name" services/drupal
 
-if test "$BUILDKITE_PULL_REQUEST" != "false"; then
+if test "$BUILDKITE_PULL_REQUEST" = "false"; then
   docker push "$image_name"
 fi
