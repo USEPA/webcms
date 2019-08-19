dd5cd150550f964d07b3738935daf83cde0f4808
gustavderdrache <aford@forumone.com>
2019-08-19 14:02:44 -0400

    EPAD8-13: Add Buildkite pipeline

---
+++ .buildkite/cf-push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CF_DOCKER_PASSWORD="$(aws --region=us-east-1 ecr get-authorization-token --output=text --query='authorizationData[].authorizationToken' | base64 --decode)"
+export CF_DOCKER_PASSWORD
+
+set -x
+
+tag="$BUILDKITE_BRANCH-$BUILDKITE_BUILD_NUMBER"
+image_name="$DOCKER_REPOSITORY:$tag"
+
+curl -s 'https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github' > \
+  cf-cli.tgz
+
+tar xzf cf-cli.tgz cf
+
+cf auth
+
+cf push --docker-username AWS --docker-image "$image_name"
+
+cf logout
---
+++ .buildkite/docker-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+tag="$BUILDKITE_BRANCH-$BUILDKITE_BUILD_NUMBER"
+image_name="$DOCKER_REPOSITORY:$tag"
+
+docker build -t "$image_name" services/drupal
+
+if test "$BUILDKITE_PULL_REQUEST" != "false"; then
+  docker push "$image_name"
+fi
