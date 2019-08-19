5dc6f35d2c0dd949ebe62319640bdb5b777c0ded
gustavderdrache <aford@forumone.com>
2019-08-19 16:14:46 -0400

    EPAD8-13: Try debugging AWS cli

--- .buildkite/docker-build.sh
+++ .buildkite/docker-build.sh
@@ -2,6 +2,8 @@
 
 set -exuo pipefail
 
+eval "$(aws --debug ecr get-login --region=us-east-1 --no-include-email)"
+
 tag="$BUILDKITE_BRANCH-$BUILDKITE_BUILD_NUMBER"
 image_name="$DOCKER_REPOSITORY:$tag"
 
