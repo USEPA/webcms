e406a201e1811835fcbb8346f5c78b6b1a47f4e1
gustavderdrache <aford@forumone.com>
2019-08-19 16:40:30 -0400

    EPAD8-13: Specify org & space

--- .buildkite/cf-push.sh
+++ .buildkite/cf-push.sh
@@ -16,8 +16,9 @@ curl -Ls 'https://packages.cloudfoundry.org/stable?release=linux64-binary&source
 tar xzf cf-cli.tgz cf
 
 ./cf api 'https://api.fr.cloud.gov'
+./cf target -o epa-prototyping -s webcms-proto
 ./cf auth
 
-./cf push --docker-username AWS --docker-image "$image_name"
+./cf push webcms-proto --docker-username AWS --docker-image "$image_name"
 
 ./cf logout
