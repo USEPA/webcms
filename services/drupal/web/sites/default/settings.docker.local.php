e19e002f38e325a3ff275e46cc27e411a19e077f
gustavderdrache <aford@forumone.com>
2019-08-19 12:17:16 -0400

    EPAD8-135: Use CNAME for Minio

--- services/drupal/web/sites/default/settings.docker.local.php
+++ services/drupal/web/sites/default/settings.docker.local.php
@@ -4,6 +4,10 @@
 $config['s3fs.settings']['use_customhost'] = TRUE;
 $config['s3fs.settings']['hostname'] = 'minio:9000';
 
+// Override output to point to localhost:8888/drupal in order to see Minio-saved files
+$config['s3fs.settings']['use_cname'] = TRUE;
+$config['s3fs.settings']['domain'] = 'localhost:8888/drupal';
+
 // Uncomment out the below line for first-time installation (in order to avoid
 // not having a redis cache backend causing weird installation errors).
 // unset($settings['cache']['default']);
