7efb0677fda481c7091f3a10ba676c0fe883c2a9
Aaron Zinck <azinck@forumone.com>
2019-08-21 14:32:02 -0400

    Allow drush to access twig cache for local dev

--- services/drupal/web/sites/default/settings.local.env.php
+++ services/drupal/web/sites/default/settings.local.env.php
@@ -8,6 +8,9 @@ $config['s3fs.settings']['hostname'] = 'minio:9000';
 $config['s3fs.settings']['use_cname'] = TRUE;
 $config['s3fs.settings']['domain'] = 'localhost:8888/drupal';
 
+// Map twig cache onto shared filesystem to allow drush to clear and write twig cache for local development.
+$settings['php_storage']['twig']['directory'] = '/var/www/html/web/sites/default/files/tmp/cache/twig';
+
 // Avoid having a redis cache backend causing errors before we've had a chance to enable the module.
 if ($env_state == 'build') {
   unset($settings['cache']['default']);
