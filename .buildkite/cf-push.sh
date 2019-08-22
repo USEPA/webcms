81ed9f793bbb4908f18547fb15192121b0b3d258
gustavderdrache <aford@forumone.com>
2019-08-22 12:56:36 -0400

    EPAD8-155: Add drush script

--- .buildkite/cf-push.sh
+++ .buildkite/cf-push.sh
@@ -2,12 +2,21 @@
 
 set -euo pipefail
 
+org_name=epa-prototyping
+app_name=webcms-proto
+
 if test "$BUILDKITE_PULL_REQUEST" != "false"; then
   echo 'Skipping deployment on pull requests'
 fi
 
 CF_DOCKER_PASSWORD="$(aws --region=us-east-1 ecr get-authorization-token --output=text --query='authorizationData[].authorizationToken' | base64 --decode | cut -d: -f2)"
 export CF_DOCKER_PASSWORD
+
+# Define a logout function - this allows us to invoke `./cf logout' even if a command trips
+# the error-exit option.
+logout() {
+  ./cf logout
+}
 
 # Only log commands after we've obtained authorization (or else the console will see the token)
 set -x
@@ -23,8 +32,12 @@ tar xzf cf-cli.tgz cf
 ./cf api 'https://api.fr.cloud.gov'
 ./cf auth
 
-./cf target -o epa-prototyping -s webcms-proto
-./cf push webcms-proto --docker-username AWS --docker-image "$image_name"
+# Register the exit handler now that we've successfully logged in.
+trap logout EXIT
 
-# Log out to ensure authorization tokens are destroyed
-./cf logout
+./cf target -o "$org_name" -s "$app_name"
+./cf push "$app_name" --docker-username AWS --docker-image "$image_name"
+
+# Re-use the image tag for this deployment: we can quickly find the task name based on
+# the build metadata.
+./cf run-task "$app_name" 'bash scripts/update.sh' --name "$tag"
---
+++ services/drupal/scripts/cloudfoundry/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is run after a successful CloudFoundry deployment. See the cf-push.sh script
+# in the .buildkite directory.
+
+set -exuo pipefail
+
+export PATH="$PATH:/var/www/html/vendor/bin"
+
+drush updb -y
+drush cim -y
+drush cr
