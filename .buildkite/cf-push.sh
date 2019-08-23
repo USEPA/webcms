a83685b00898390ca56581bebbb7f0923f94a977
gustavderdrache <aford@forumone.com>
2019-08-23 09:42:00 -0400

    EPAD8-155: Capture logs on failed build

--- .buildkite/cf-push.sh
+++ .buildkite/cf-push.sh
@@ -24,6 +24,8 @@ set -x
 tag="$BUILDKITE_BRANCH-$BUILDKITE_BUILD_NUMBER"
 image_name="$DOCKER_REPOSITORY:$tag"
 
+echo '--- Authenticating'
+
 curl -Ls 'https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github' > \
   cf-cli.tgz
 
@@ -36,7 +38,22 @@ tar xzf cf-cli.tgz cf
 trap logout EXIT
 
 ./cf target -o "$org_name" -s "$app_name"
-./cf push "$app_name" --docker-username AWS --docker-image "$image_name"
+
+echo '--- Deploying'
+
+# Run app push - if CloudFoundry reports a failure, capture logs and upload them to Buildkite.
+if ! ./cf push "$app_name" --docker-username AWS --docker-image "$image_name"; then
+  exit_code=$?
+
+  cf logs "$app_name" --recent > cf-recent.txt
+  buildkite-agent artifact upload cf-recent.txt
+
+  echo "^^^ +++"
+  echo "Build failed. Logs have been uploaded." > /dev/stderr
+  exit "$exit_code"
+fi
+
+echo '--- Running Drush'
 
 # Re-use the image tag for this deployment: we can quickly find the task name based on
 # the build metadata.
