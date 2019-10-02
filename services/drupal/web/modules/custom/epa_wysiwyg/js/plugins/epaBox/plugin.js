ec99973cb636049d5047c022035e0996067f719e
Olu Adeyanju <oadeyanju@forumone.com>
2019-10-02 16:12:26 -0700

    [EPAD8-114] Add Related Info Box wysiwyg plugin

---
+++ services/drupal/web/modules/custom/epa_wysiwyg/js/plugins/epaBox/plugin.js
@@ -0,0 +1,99 @@
+(function (CKEDITOR) {
+  function StringBuffer() {
+    this.buffer = [];
+  }
+
+  StringBuffer.prototype.append = function append(string) {
+    this.buffer.push(string);
+    return this;
+  };
+
+  StringBuffer.prototype.toString = function toString() {
+    return this.buffer.join('');
+  };
+
+  CKEDITOR.plugins.add('epaBox', {
+    requires: ['button', 'dialog'],
+
+    init: function(editor) {
+      var iconPath = this.path + 'images/epaBox.png';
+
+      editor.addCommand('epaBoxCommand', new CKEDITOR.dialogCommand('epaBoxDialog'));
+
+      editor.ui.addButton('epaBoxButton', {
+        label: 'Related Info Box',
+        command: 'epaBoxCommand',
+        icon: iconPath
+      });
+
+      CKEDITOR.dialog.add('epaBoxDialog', function(editor) {
+        return {
+          title: 'EPA Box',
+          minWidth: 400,
+          minHeight: 200,
+          contents: [{
+            id: 'general',
+            label: 'Settings',
+            elements: [{
+              type: 'select',
+              id: 'float',
+              label: 'Position:',
+              validate: CKEDITOR.dialog.validate.notEmpty("You must select the position of the box."),
+              items: [
+                ['Floated Left', 'left'],
+                ['Floated Right', 'right']
+              ],
+              default: 'right',
+              commit : function(data) {
+                data.position = this.getValue();
+              }
+            },
+            {
+              type: 'text',
+              id: 'title',
+              label: 'Box Title:',
+              commit : function(data) {
+                data.title = this.getValue();
+              }
+            }]
+          }],
+          onOk: function() {
+            var dialog = this,
+                data = {},
+                e = editor.getSelection().getSelectedElement(),
+                text = "",
+                box = new StringBuffer();
+
+            this.commitContent(data);
+
+            if (e === null) {
+              text = editor.getSelection().getSelectedText();
+            }
+
+            box.append('<div class="box multi related-info ' + data.position + '">');
+
+            if (data.title !== '') {
+              box.append('<h2 class="pane-title">' + data.title + '</h2>');
+            }
+
+            box.append('<div class="pane-content">');
+
+            if (e !== null) {
+              box.append(e.getHtml());
+            }
+            else if (text.length > 1) {
+              box.append('<p>' + text + '</p>');
+            }
+            else {
+              box.append('<p>Enter your box content here.</p>');
+            }
+
+            box.append('</div>');
+
+            editor.insertHtml(box.toString());
+          }
+        };
+      });
+    }
+  });
+})(CKEDITOR);
