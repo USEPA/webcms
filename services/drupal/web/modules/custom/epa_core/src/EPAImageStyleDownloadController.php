19887a98519b8850d8f4d1a662738895823426ab
Olu Adeyanju <oadeyanju@forumone.com>
2020-02-11 10:00:14 -0800

    [EPAD8-300] Refactor to accomodate image style access

--- services/drupal/web/modules/custom/epa_core/src/EPAImageStyleDownloadController.php
+++ services/drupal/web/modules/custom/epa_core/src/EPAImageStyleDownloadController.php
@@ -2,15 +2,18 @@
 
 namespace Drupal\epa_core;
 
+use Drupal\Core\Image\ImageFactory;
+use Drupal\Core\Lock\LockBackendInterface;
 use Drupal\Core\StreamWrapper\StreamWrapperManagerInterface;
-use Drupal\system\FileDownloadController;
+use Drupal\image\Controller\ImageStyleDownloadController;
+use Drupal\image\ImageStyleInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Extend system FileDownloadController.
  */
-class EPAFileDownloadController extends FileDownloadController {
+class EPAImageStyleDownloadController extends ImageStyleDownloadController {
 
   /**
    * EPAFileDownload service.
@@ -24,11 +27,15 @@ class EPAFileDownloadController extends FileDownloadController {
    *
    * @param \Drupal\epa_core\EPAFileDownload $epa_file_download
    *   The epaFileDownload service.
+   * @param \Drupal\Core\Lock\LockBackendInterface $lock
+   *   The lock backend.
+   * @param \Drupal\Core\Image\ImageFactory $image_factory
+   *   The image factory.
    * @param \Drupal\Core\StreamWrapper\StreamWrapperManagerInterface $stream_wrapper_manager
    *   The stream wrapper manager.
    */
-  public function __construct(EPAFileDownload $epa_file_download, StreamWrapperManagerInterface $stream_wrapper_manager = NULL) {
-    parent::__construct($stream_wrapper_manager);
+  public function __construct(EPAFileDownload $epa_file_download, LockBackendInterface $lock, ImageFactory $image_factory, StreamWrapperManagerInterface $stream_wrapper_manager = NULL) {
+    parent::__construct($lock, $image_factory, $stream_wrapper_manager);
     $this->epaFileDownload = $epa_file_download;
   }
 
@@ -38,6 +45,8 @@ class EPAFileDownloadController extends FileDownloadController {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('epa_core.private_file_download'),
+      $container->get('lock'),
+      $container->get('image.factory'),
       $container->get('stream_wrapper_manager')
     );
   }
@@ -48,9 +57,9 @@ class EPAFileDownloadController extends FileDownloadController {
    * If current user is anonymous, make a decision about whether to
    * serve file based on the associated media.
    */
-  public function download(Request $request, $scheme = 'private') {
+  public function deliver(Request $request, $scheme, ImageStyleInterface $image_style) {
     $this->epaFileDownload->privateFileCheck($request, $scheme);
-    $response = parent::download($request, $scheme);
+    $response = parent::deliver($request, $scheme, $image_style);
     return $response;
   }
 
--- services/drupal/web/modules/custom/epa_core/src/Routing/RouteSubscriber.php
+++ services/drupal/web/modules/custom/epa_core/src/Routing/RouteSubscriber.php
@@ -14,11 +14,14 @@ class RouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
-    $route_names = ['system.files', 'system.private_file_download'];
-    foreach ($route_names as $route_name) {
+    $private_file_routes = ['system.files', 'system.private_file_download'];
+    foreach ($private_file_routes as $route_name) {
       if ($route = $collection->get($route_name)) {
         $route->setDefault('_controller', '\Drupal\epa_core\EPAFileDownloadController::download');
       }
+    }
+    if ($route = $collection->get('image.style_private')) {
+      $route->setDefault('_controller', '\Drupal\epa_core\EPAImageStyleDownloadController::deliver');
     }
   }
 
