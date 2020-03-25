e9bf76d4e2f690dcdb696d40cc536fb9a011a252
Brian Tofte-Schumacher <btofte-schumacher@forumone.com>
2020-03-25 12:20:44 -0700

    [EPAD8-276] Provide main_col and sidebar pane data

--- services/drupal/web/modules/custom/epa_migrations/src/Plugin/migrate/source/EpaNode.php
+++ services/drupal/web/modules/custom/epa_migrations/src/Plugin/migrate/source/EpaNode.php
@@ -25,18 +25,14 @@ class EpaNode extends Node {
     }
 
     // To prepare rows for import into fields, we're going to:
+    // - Skip nodes that use panelizer and have a layout other than onecol_page
+    //   or twocol_page.
+    // - Add a 'layout' source property to populate the 'field_layout'.
+    // - Add source properties containing query results for 'main_col' and
+    //   'sidebar' panes.
     //
-    // 1. Skip nodes that use panelizer and have a layout other than
-    // onecol_page or twocol_page.
-    //
-    // 2. Add a 'did' source property that can be used during the process phase.
-    //
-    // 3. Add a 'layout' source property to populate the 'field_layout'.
-    //
-    // First, initialize the 'did' and 'layout' source properties as NULL so we
-    // can properly process nodes that do not have a record in the
-    // 'panelizer_entith' table.
-    $row->setSourceProperty('did', NULL);
+    // First, initialize the 'layout' source property as NULL so we can properly
+    // process nodes that do not have a record in the 'panelizer_entith' table.
     $row->setSourceProperty('layout', NULL);
 
     // Get the Display ID for the current revision.
@@ -57,17 +53,45 @@ class EpaNode extends Node {
       if (!in_array($layout, ['onecol_page', 'twocol_page'])) {
         // Skip this row if this node uses a panelizer layout other than
         // onecol_page or twocol_page. We'll migrate these nodes with the
-        // epa_panelizer source plugin.
+        // epa_panelizer_node source plugin.
         return FALSE;
       }
       else {
-        // Update the 'did' and 'layout' properties to their stored values.
-        $row->setSourceProperty('did', $did);
+        // Update the 'layout' property to its actual value.
         $row->setSourceProperty('layout', $layout);
+
+        // Fetch the main_col panes and add the result as a source property.
+        $main_col_panes = $this->fetchPanes('main_col', $did);
+        $row->setSourceProperty('main_col_panes', $main_col_panes);
+
+        // Fetch the sidebar panes and add the result as a source property.
+        $sidebar_panes = $this->fetchPanes('sidebar', $did);
+        $row->setSourceProperty('sidebar_panes', $sidebar_panes);
+
       }
     }
 
     parent::prepareRow($row);
+  }
+
+  /**
+   * Given a panel machine name and did, fetch panes.
+   *
+   * @param string $panel
+   *   The machine name of the panel from which to select panes.
+   * @param int $did
+   *   The Display ID for this node.
+   *
+   * @return \Drupal\Core\Database\StatementInterface|null
+   *   A prepared statement, or NULL if the query is not valid.
+   */
+  private function fetchPanes($panel, $did) {
+    return $this->select('panels_pane', 'pp')
+      ->fields('pp')
+      ->condition('pp.did', $did)
+      ->condition('pp.panel', $panel)
+      ->execute()
+      ->fetchAll();
   }
 
 }
