c1899011c55f6389f47cc3a28327ddc83fadc7ad
Aaron Zinck <azinck@gmail.com>
2020-03-27 12:06:34 -0400

    Fix up the field formatter a bit...get the formatter to be recognized EPAD8-117

--- services/drupal/web/modules/custom/epa_wysiwyg/src/Plugin/Field/FieldFormatter/EpaExtensionFileFormatter.php
+++ services/drupal/web/modules/custom/epa_wysiwyg/src/Plugin/Field/FieldFormatter/EpaExtensionFileFormatter.php
@@ -1,12 +1,9 @@
 <?php
 
-
 namespace Drupal\epa_wysiwyg\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\file\Plugin\Field\FieldFormatter\FileFormatterBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'file_with_extension' formatter.
@@ -19,37 +16,12 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   }
  * )
  */
-class EPAExtensionFileFormatter extends FileFormatterBase
-{
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings)
-  {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
-  }
+class EpaExtensionFileFormatter extends FileFormatterBase {
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
-  {
-    return new static(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['label'],
-      $configuration['view_mode'],
-      $configuration['third_party_settings']
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function viewElements(FieldItemListInterface $items, $langcode)
-  {
+  public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
 
     foreach ($this->getEntitiesToView($items, $langcode) as $delta => $file) {
