8ad39f4dea87f770f0c5b1dc374da0b9aff7e570
gustavderdrache <aford@forumone.com>
2020-04-02 16:11:47 -0400

    EPAD8-550: Replace parameters with secrets

--- infrastructure/terraform/parameters.tf
+++ infrastructure/terraform/parameters.tf
@@ -1,143 +1,36 @@
-# NB. Do NOT change the passwords here in this file. Terraform stores all of its state
-# values - even sensitive ones - in cleartext. Instead, use the Parameter Store dashboard
-# to change them. Remember to force a restart of the ECS tasks once you've done so.
+# Secrets in this file are not initialized; remember to populate them on the first run of
+# the Terraform template.
 
-# Parameters for the MySQL administrator user, prefixed at /webcms/db_root
-
-resource "aws_ssm_parameter" "db_root_username" {
-  name        = "/webcms/db_root/username"
-  description = "Username for the WebCMS DB administrator"
-  value       = var.db-username
-  type        = "String"
-
-  tags = {
-    Application = "WebCMS"
-  }
-}
-
-resource "aws_ssm_parameter" "db_root_password" {
+resource "aws_secretsmanager_secret" "db_root_password" {
   name        = "/webcms/db_root/password"
-  description = "Password for the WebCMS DB administrato"
-  value       = var.db-password
-  type        = "SecureString"
-
-  tags = {
-    Application = "WebCMS"
-  }
-
-  # Ignore changes to this password parameter: we only create this here because we need
-  # its ARN in other resources.
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-# Parameters for the WebCMS's database, including the database name and user credentials.
-# Note that these are NOT used to automatically create the user in MySQL. Someone will
-# need to perform the initial database and user creation for Drupal after this cluster
-# has been created for the first time.
-
-resource "aws_ssm_parameter" "db_app_username" {
-  name        = "/webcms/db_app/username"
-  description = "Username for the WebCMS DB user"
-  value       = local.database-name
-  type        = "String"
+  description = "Password for the WebCMS DB administrator"
 
   tags = {
     Application = "WebCMS"
   }
 }
 
-resource "aws_ssm_parameter" "db_app_password" {
+resource "aws_secretsmanager_secret" "db_app_password" {
   name        = "/webcms/db_app/password"
   description = "Password for the WebCMS DB user"
-  value       = "changeme"
-  type        = "SecureString"
-
-  tags = {
-    Application = "WebCMS"
-  }
-
-  # Ignore changes to this password parameter: we only create this here because we need
-  # its ARN in other resources.
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "aws_ssm_parameter" "db_app_database" {
-  name        = "/webcms/db_app/database"
-  description = "Name of the WebCMS DB schema"
-  value       = local.database-user
-  type        = "String"
 
   tags = {
     Application = "WebCMS"
   }
 }
 
-resource "aws_ssm_parameter" "hash_salt" {
+resource "aws_secretsmanager_secret" "hash_salt" {
   name        = "/webcms/drupal/hash_salt"
   description = "Drupal hash salt"
-  value       = "changeme"
-  type        = "SecureString"
-
-  tags = {
-    Application = "WebCMS"
-  }
-
-  # Ignore changes to this sensitive parameter: as with other sensitive values, this is
-  # only here because we need the ARN.
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-# Mail-related parameters
-
-resource "aws_ssm_parameter" "mail_user" {
-  name        = "/webcms/mail/username"
-  description = "Username for SMTP auth"
-  value       = var.email-auth-user
-  type        = "String"
 
   tags = {
     Application = "WebCMS"
   }
 }
 
-resource "aws_ssm_parameter" "mail_pass" {
+resource "aws_secretsmanager_secret" "mail_pass" {
   name        = "/webcms/mail/password"
   description = "Password for SMTP auth"
-  value       = "changeme"
-  type        = "SecureString"
-
-  tags = {
-    Application = "WebCMS"
-  }
-
-  # Another sensitive parameter
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "aws_ssm_parameter" "mail_from" {
-  name        = "/webcms/mail/from"
-  description = "From address for site email"
-  value       = var.email-from
-  type        = "String"
-
-  tags = {
-    Application = "WebCMS"
-  }
-}
-
-resource "aws_ssm_parameter" "mail_host" {
-  name        = "/webcms/mail/host"
-  description = "SMTP hostname to connect to"
-  value       = var.email-host
-  type        = "String"
 
   tags = {
     Application = "WebCMS"
