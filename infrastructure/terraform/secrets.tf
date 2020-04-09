d032888332f231e858290e143eba2bf4a61e45b0
gustavderdrache <aford@forumone.com>
2020-04-09 11:28:40 -0400

    Work around secret uniqueness requirements

--- infrastructure/terraform/secrets.tf
+++ infrastructure/terraform/secrets.tf
@@ -1,8 +1,22 @@
 # Secrets in this file are not initialized; remember to populate them on the first run of
 # the Terraform template.
 
+# When we use fixed names in Secrets Manager, we can run into an issue where Terraform can't
+# destroy the secret until the default time window has passed. This means that we will need
+# to generate a unique suffix ourselves. Whenever the Terraform plan indicates that a secret
+# will need to be replaced, simply taint this secret_suffix resource and Terraform will
+# generate a new name for us.
+#
+# NB. When this happens, an admin will need to copy the secrets values over from the old
+# secrets to the new secrets. Be sure to take a backup of the values BEFORE applying with
+# Terraform, or else the secret values will be lost.
+resource "random_pet" "secret_suffix" {
+  # We use length=2 to follow the Docker convention
+  length = 2
+}
+
 resource "aws_secretsmanager_secret" "db_root_password" {
-  name        = "/webcms/db_root/password"
+  name        = "/webcms-${random_pet.secret_suffix}/db_root/password"
   description = "Password for the WebCMS DB administrator"
 
   tags = {
@@ -11,7 +25,7 @@ resource "aws_secretsmanager_secret" "db_root_password" {
 }
 
 resource "aws_secretsmanager_secret" "db_app_password" {
-  name        = "/webcms/db_app/password"
+  name        = "/webcms-${random_pet.secret_suffix}/db_app/password"
   description = "Password for the WebCMS DB user"
 
   tags = {
@@ -20,7 +34,7 @@ resource "aws_secretsmanager_secret" "db_app_password" {
 }
 
 resource "aws_secretsmanager_secret" "hash_salt" {
-  name        = "/webcms/drupal/hash_salt"
+  name        = "/webcms-${random_pet.secret_suffix}/drupal/hash_salt"
   description = "Drupal hash salt"
 
   tags = {
@@ -29,7 +43,7 @@ resource "aws_secretsmanager_secret" "hash_salt" {
 }
 
 resource "aws_secretsmanager_secret" "mail_pass" {
-  name        = "/webcms/mail/password"
+  name        = "/webcms-${random_pet.secret_suffix}/mail/password"
   description = "Password for SMTP auth"
 
   tags = {
