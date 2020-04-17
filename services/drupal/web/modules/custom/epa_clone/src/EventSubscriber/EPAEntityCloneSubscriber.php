0e0c8f7d12005b6bb55d165665dde8392b5833d1
Aaron Zinck <azinck@gmail.com>
2020-04-17 13:48:01 -0400

    EPAD8-516 refactor to reduce number of entity saves and ensure cloned indicator is set properly on frequent questions

--- services/drupal/web/modules/custom/epa_clone/src/EventSubscriber/EPAEntityCloneSubscriber.php
+++ services/drupal/web/modules/custom/epa_clone/src/EventSubscriber/EPAEntityCloneSubscriber.php
@@ -11,62 +11,49 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class EPAEntityCloneSubscriber implements EventSubscriberInterface {
 
   public static function getSubscribedEvents() {
-    $events[EntityCloneEvents::POST_CLONE][] = ['formatClonedSignifier', -1000];
-    $events[EntityCloneEvents::POST_CLONE][] = ['generateGroupContent', -1000];
-    $events[EntityCloneEvents::POST_CLONE][] = ['clearMachineName', -1000];
+    $events[EntityCloneEvents::POST_CLONE][] = ['postClone', -1000];
     return $events;
   }
 
   /**
-   * - Prepend a "Cloned: " signifier to the title of cloned nodes.
-   * - Remove the appended " - Cloned" signifier to the title of cloned nodes.
    *
    * @param \Drupal\entity\Event\EntityCloneEvent $event
    *   The entity_clone event.
    */
-  public function formatClonedSignifier(EntityCloneEvent $event) {
+  public function postClone(EntityCloneEvent $event) {
     $cloned_entity = $event->getClonedEntity();
+
+   // Prepend a "Cloned: " signifier to the title of cloned nodes.
+   // Remove the appended " - Cloned" signifier to the title of cloned nodes.
     if ($cloned_entity instanceof Node) {
       $old_signifier = ' - Cloned';
       $new_signifier = 'Cloned: ';
-      $old_pos = strpos($cloned_entity->getTitle(), $old_signifier);
-      $new_title = $old_pos !== FALSE ?
-        $new_signifier . substr($cloned_entity->getTitle(), 0, $old_pos) :
-        $new_signifier . $cloned_entity->getTitle();
-      $cloned_entity->setTitle($new_title);
+
       if ($cloned_entity->bundle() == 'faq') {
-        $cloned_entity->set('field_question', $new_title);
+        $old = $cloned_entity->field_question->getString();
+        $cloned_entity->field_question->setValue($new_signifier. ' '. $old);
       }
+      else {
+        $old_pos = strpos($cloned_entity->getTitle(), $old_signifier);
+        $new_title = $old_pos !== FALSE ?
+          $new_signifier . substr($cloned_entity->getTitle(), 0, $old_pos) :
+          $new_signifier . $cloned_entity->getTitle();
+        $cloned_entity->setTitle($new_title);
+      }
+      $original_entity = $event->getEntity();
+
+      // Clear the machine name field on cloned entities
+      if ($original_entity->hasField('field_machine_name') && !empty($original_entity->get('field_machine_name'))) {
+        $cloned_entity->set('field_machine_name', '');
+      }
+
+      // Ensure the new node is assigned to the same group as the old one
+      $groups = \Drupal::service('epa_web_areas.web_areas_helper')->getNodeReferencingGroups($original_entity);
+      foreach ($groups as $group) {
+        $group->addContent($cloned_entity, 'group_' . $original_entity->getEntityTypeId() . ':' . $original_entity->bundle());
+      }
+
       $cloned_entity->save();
     }
-  }
-
-  /**
-   * - Assign content to its respective group after cloning.
-   *
-   * @param \Drupal\entity\Event\EntityCloneEvent $event
-   *   The entity_clone event.
-   */
-  public function generateGroupContent(EntityCloneEvent $event) {
-    $entity = $event->getEntity();
-    $cloned_entity = $event->getClonedEntity();
-    $groups = \Drupal::service('epa_web_areas.web_areas_helper')->getNodeReferencingGroups($entity);
-    foreach ($groups as $group) {
-      $group->addContent($cloned_entity, 'group_' . $entity->getEntityTypeId() . ':' . $entity->bundle());
-    }
-  }
-
-  /**
-   * - Clear the machine name field on the cloned entity.
-   * @param \Drupal\entity_clone\Event\EntityCloneEvent $event
-   *   The entity_clone event.
-   */
-  public function clearMachineName(EntityCloneEvent $event) {
-    $entity = $event->getEntity();
-    $cloned_entity = $event->getClonedEntity();
-    if ($entity->getEntityTypeId() === 'node' && $entity->hasField('field_machine_name') && !empty($entity->get('field_machine_name'))) {
-      $cloned_entity->set('field_machine_name', '');
-    }
-    $cloned_entity->save();
   }
 }
