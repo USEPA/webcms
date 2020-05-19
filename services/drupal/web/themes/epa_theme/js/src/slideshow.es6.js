f49bd2b9ec68cdbe89f61ed4f8755d5045cca24b
Dan Mouyard <dcmouyard@gmail.com>
2020-05-19 14:29:28 -0400

    EPAD8-270: Update slideshow JS documentation

--- services/drupal/web/themes/epa_theme/js/src/slideshow.es6.js
+++ services/drupal/web/themes/epa_theme/js/src/slideshow.es6.js
@@ -1,8 +1,6 @@
-// Custom scripts file
-// Include the USWDS Accordion script.
-// This makes the component available globally. If you're only using it on certain pages,
-// include it on a template-specific script file instead.
-// Be sure to initialize any components as well (see init() function below.)
+// Slideshow script
+// Implements Tiny Slider library
+// https://github.com/ganlanyuan/tiny-slider
 import Drupal from 'drupal';
 import { tns } from 'tiny-slider/src/tiny-slider';
 
