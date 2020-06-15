db5876686c4346e54566b1ab116e319a3e414d81
gustavderdrache <aford@forumone.com>
2020-06-15 14:07:00 -0400

    EPAD8-622: Enforce TLS for Elasticsearch

--- infrastructure/terraform/search.tf
+++ infrastructure/terraform/search.tf
@@ -36,7 +36,7 @@ resource "aws_elasticsearch_domain" "es" {
   }
 
   domain_endpoint_options {
-    enforce_https       = false
+    enforce_https       = true
     tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
