d2620c6f1b72969fa0695ef9fade0797adb69571
Melissa Bent <mbent@forumone.com>
2020-07-06 14:51:54 -0600

    [EPAD8-704] Update ES config in settings.php to correct invalid array pointer.

--- services/drupal/web/sites/default/settings.php
+++ services/drupal/web/sites/default/settings.php
@@ -822,7 +822,7 @@ if ($env_state === 'run') {
 // We don't authenticate with HTTP auth; we instead inject AWS SDK signatures when a request
 // is made.
 $config['elasticsearch_connector.cluster.elasticsearch']['url'] = getenv('WEBCMS_SEARCH_HOST');
-$config['elasticsearch_connector.cluster.elasticsearch']['use_authentication'] = FALSE;
+$config['elasticsearch_connector.cluster.elasticsearch']['options']['use_authentication'] = FALSE;
 
 $config['smtp.settings']['smtp_username'] = getenv('WEBCMS_MAIL_USER');
 $config['smtp.settings']['smtp_password'] = getenv('WEBCMS_MAIL_PASS');
