9343c5e35dc9710a0da5a3a747b3bc83dd113205
Brian Tofte-Schumacher <btofte-schumacher@forumone.com>
2020-07-20 15:23:23 -0700

    [EPAD8-728] Add check to make sure there's review data

--- services/drupal/web/modules/custom/epa_migrations/src/Plugin/migrate/source/EpaNode.php
+++ services/drupal/web/modules/custom/epa_migrations/src/Plugin/migrate/source/EpaNode.php
@@ -41,11 +41,13 @@ class EpaNode extends Node {
     $import_date = strtotime('today');
     $end_import_date = strtotime('+30 days', $import_date);
     $review_deadline = \DateTime::createFromFormat('Y-m-d H:i:s', $review_deadline, new \DateTimeZone('America/New_York'));
-    if ($review_deadline->getTimestamp() >= $import_date && $review_deadline->getTimestamp() <= $end_import_date) {
-      $review_deadline->setTimestamp(strtotime('+30 days', $review_deadline->getTimestamp()));
-    }
+    if ($review_deadline) {
+      if ($review_deadline->getTimestamp() >= $import_date && $review_deadline->getTimestamp() <= $end_import_date) {
+        $review_deadline->setTimestamp(strtotime('+30 days', $review_deadline->getTimestamp()));
+      }
 
-    $row->setSourceProperty('field_modified_review_deadline', [0 => ['value' => $review_deadline->format('Y-m-d H:i:s')]]);
+      $row->setSourceProperty('field_modified_review_deadline', [0 => ['value' => $review_deadline->format('Y-m-d H:i:s')]]);
+    }
 
     // Get the revision moderation state and timestamp.
     $state_data = $this->select('node_revision_epa_states', 'nres')
