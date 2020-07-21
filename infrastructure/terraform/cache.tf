c34090830893f7f4f2bb114b3458ad9c47aad3c2
gustavderdrache <aford@forumone.com>
2020-07-21 13:54:15 -0400

    Assign security group to elasticache

--- infrastructure/terraform/cache.tf
+++ infrastructure/terraform/cache.tf
@@ -10,7 +10,8 @@ resource "aws_elasticache_cluster" "cache" {
   num_cache_nodes      = var.cache-replica-count
   parameter_group_name = "default.memcached1.5"
 
-  subnet_group_name = aws_elasticache_subnet_group.default.name
+  security_group_ids = [aws_security_group.cache.id]
+  subnet_group_name  = aws_elasticache_subnet_group.default.name
 
   tags = merge(local.common-tags, {
     Name = "${local.name-prefix} Cache"
