78c77385f6a26528bd6b889ba2363fe9cf8ad255
Brian Tofte-Schumacher <btofte-schumacher@forumone.com>
2020-08-07 15:09:58 -0700

    [EPAD8-817] Migrate group menu links

---
+++ services/drupal/web/modules/custom/epa_migrations/src/Plugin/migrate/source/EpaOgMenuLink.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\epa_migrations\Plugin\migrate\source;
+
+use Drupal\menu_link_content\Plugin\migrate\source\MenuLink;
+use Drupal\migrate\Row;
+
+/**
+ * Drupal menu link source from database.
+ *
+ * @MigrateSource(
+ *   id = "epa_og_menu_link",
+ * )
+ */
+class EpaOgMenuLink extends MenuLink {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = parent::query();
+
+    $query->condition('ml.menu_name', 'menu-og-%', 'LIKE');
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    // Change menu name from 'menu-og-id' to 'group-id-menu'.
+    $pattern = '/menu-og-(\d+)/';
+    $replacement = 'group-${1}-menu';
+    $row->setSourceProperty('d8_menu_name', preg_replace($pattern, $replacement, $row->getSourceProperty('menu_name')));
+
+    return parent::prepareRow($row);
+  }
+
+}
