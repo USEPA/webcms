48218a42a862ac18fea27c62a29141464bcdc33e
Brian Tofte-Schumacher <btofte-schumacher@forumone.com>
2020-09-08 13:03:39 -0700

    [EPAD8-856] Simplify logic, let the parent handle the rest

--- services/drupal/web/modules/custom/epa_links/src/Plugin/Field/FieldFormatter/LinkWithEntityTitleOrLinkTextFormatter.php
+++ services/drupal/web/modules/custom/epa_links/src/Plugin/Field/FieldFormatter/LinkWithEntityTitleOrLinkTextFormatter.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\epa_links\Plugin\Field\FieldFormatter;
 
-use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
@@ -85,16 +84,10 @@ class LinkWithEntityTitleOrLinkTextFormatter extends LinkFormatter {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    // Replicate the logic in LinkFormatter, but load the linked entity's title
-    // if the link is an internal route.
-    $element = [];
-    $entity = $items->getEntity();
     $settings = $this->getSettings();
 
-    foreach ($items as $delta => $item) {
-      // By default use the full URL as the link text.
+    foreach ($items as $item) {
       $url = $this->buildUrl($item);
-      $link_title = $url->toString();
 
       // If the url is internal, try to get the linked entity's title.
       if (empty($settings['url_only']) && $url->isRouted()) {
@@ -102,57 +95,12 @@ class LinkWithEntityTitleOrLinkTextFormatter extends LinkFormatter {
         $entity_type = key($url_params);
         $entity = $this->entityTypeManager->getStorage($entity_type)->load($url_params[$entity_type]);
         if ($entity && $entity->label()) {
-          $link_title = $entity->label();
-        }
-      }
-
-      // If the title field value is available, use it for the link text.
-      if (empty($settings['url_only']) && !empty($item->title)) {
-        // Unsanitized token replacement here because the entire link title
-        // gets auto-escaped during link generation in
-        // \Drupal\Core\Utility\LinkGenerator::generate().
-        $link_title = \Drupal::token()->replace($item->title, [$entity->getEntityTypeId() => $entity], ['clear' => TRUE]);
-      }
-
-      // Trim the link text to the desired length.
-      if (!empty($settings['trim_length'])) {
-        $link_title = Unicode::truncate($link_title, $settings['trim_length'], FALSE, TRUE);
-      }
-
-      if (!empty($settings['url_only']) && !empty($settings['url_plain'])) {
-        $element[$delta] = [
-          '#plain_text' => $link_title,
-        ];
-
-        if (!empty($item->_attributes)) {
-          // Piggyback on the metadata attributes, which will be placed in the
-          // field template wrapper, and set the URL value in a content
-          // attribute.
-          // @todo Does RDF need a URL rather than an internal URI here?
-          // @see \Drupal\Tests\rdf\Kernel\Field\LinkFieldRdfaTest.
-          $content = str_replace('internal:/', '', $item->uri);
-          $item->_attributes += ['content' => $content];
-        }
-      }
-      else {
-        $element[$delta] = [
-          '#type' => 'link',
-          '#title' => $link_title,
-          '#options' => $url->getOptions(),
-        ];
-        $element[$delta]['#url'] = $url;
-
-        if (!empty($item->_attributes)) {
-          $element[$delta]['#options'] += ['attributes' => []];
-          $element[$delta]['#options']['attributes'] += $item->_attributes;
-          // Unset field item attributes since they have been included in the
-          // formatter output and should not be rendered in the field template.
-          unset($item->_attributes);
+          $item->title = $entity->label();
         }
       }
     }
 
-    return $element;
+    return parent::viewElements($items, $langcode);
   }
 
 }
