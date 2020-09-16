fd2e6f6b9953f881199dd7dabe5d91288bc29faa
Dan Mouyard <dcmouyard@gmail.com>
2020-09-16 11:36:16 -0400

    EPAD8-750: Check if there is a toggle button before adding click handler

--- services/drupal/web/themes/epa_theme/js/src/toggle-admin.es6.js
+++ services/drupal/web/themes/epa_theme/js/src/toggle-admin.es6.js
@@ -4,25 +4,26 @@ import Drupal from 'drupal';
 (function(Drupal) {
   Drupal.behaviors.toggleAdmin = {
     attach(context) {
-      // const headerSearch = context.querySelectorAll('.l-header__search');
       const toggleButton = context.querySelector('.js-toggle-admin');
       const adminContent = context.querySelectorAll(
         '.usa-alert, .button-group--base[aria-label="Primary tasks"], #content-moderation-entity-moderation-form'
       );
 
-      toggleButton.addEventListener('click', event => {
-        // Toggle button text when clicked.
-        if (toggleButton.innerHTML === 'Hide Admin Info') {
-          toggleButton.innerHTML = 'Show Admin Info';
-        } else {
-          toggleButton.innerHTML = 'Hide Admin Info';
-        }
+      if (toggleButton !== null) {
+        toggleButton.addEventListener('click', event => {
+          // Toggle button text when clicked.
+          if (toggleButton.innerHTML === 'Hide Admin Info') {
+            toggleButton.innerHTML = 'Show Admin Info';
+          } else {
+            toggleButton.innerHTML = 'Hide Admin Info';
+          }
 
-        // Show/hide admin content when toggle button clicked.
-        adminContent.forEach(content => {
-          content.classList.toggle('u-hidden');
+          // Show/hide admin content when toggle button clicked.
+          adminContent.forEach(content => {
+            content.classList.toggle('u-hidden');
+          });
         });
-      });
+      }
     },
   };
 })(Drupal);
