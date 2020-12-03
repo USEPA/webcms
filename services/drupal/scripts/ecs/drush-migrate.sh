ce2570e0c0c3c81e840df91ef3aaff453aac0284
Steve Clay <steve@mrclay.org>
2020-12-03 09:38:33 -0500

    Allow setting allowable unprocessed counts without a build

--- services/drupal/scripts/ecs/drush-migrate.sh
+++ services/drupal/scripts/ecs/drush-migrate.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Note: First use allow-unprocessed.sh to set allowable unprocessed counts
+# before usage. These are stored in the D8 DB.
+
 set -euo pipefail
 
 # Number of items to process in a single batch run
 batch_size=1000
+
+# https://stackoverflow.com/a/33248547/3779
+trim() {
+  local s2 s="$*"
+  until s2="${s#[[:space:]]}"; [ "$s2" = "$s" ]; do s="$s2"; done
+  until s2="${s%[[:space:]]}"; [ "$s2" = "$s" ]; do s="$s2"; done
+  echo "$s"
+}
 
 # USAGE:
 #   run_migration MIGRATION
@@ -12,7 +23,7 @@ batch_size=1000
 #
 # This function runs a migration to completion, using the configured $batch_size variable
 # above. If a migration yields unprocessed entities greater than the threshold specified
-# in the expected_unprocessed array, then this function returns a non-zero exit code.
+# by the allow-unprocessed.sh script, then this function returns a non-zero exit code.
 run_migration() {
   local migration
   local total batches unprocessed remaining
@@ -24,8 +35,8 @@ run_migration() {
   migration="$1"
 
   # If already done, skip.
-  unprocessed="$(drush ms "$migration" --field=unprocessed)"
-  if [[ "$unprocessed" =~ ^0 ]]; then
+  unprocessed="$(trim $(drush ms "$migration" --field=unprocessed))"
+  if test "$unprocessed" = 0; then
     echo "[$migration] Bypassing, 0 remaining"
     return 0
   fi
@@ -52,10 +63,10 @@ run_migration() {
 
     # Check if we've asked to halt. Migration's own status field isn't really reliable
     # for discerning whether a migration has been stopped or completed.
-    halted="$(drush state-get epa.migrations_halted)"
+    halted="$(trim $(drush state-get epa.migrations_halted))"
     if test -n "$halted"; then
       # Output the num remaining and exit
-      remaining="$(drush ms "$migration" --field=unprocessed)"
+      remaining="$(trim $(drush ms "$migration" --field=unprocessed))"
       echo "[$migration] Halted ($remaining unprocessed of $unprocessed)"
       echo "Halting migration script, deleting halt signal"
       drush state-del epa.migrations_halted
@@ -68,11 +79,12 @@ run_migration() {
   time=$((finish - start))
 
   # Determine how many unprocessed items were left behind by the migration.
-  remaining="$(drush ms "$migration" --field=unprocessed)"
-  total="$(drush ms "$migration" --field=total)"
+  remaining="$(trim $(drush ms "$migration" --field=unprocessed))"
+  total="$(trim $(drush ms "$migration" --field=total))"
 
   # If we don't have any special expected count, default to zero.
-  expected="${expected_unprocessed[$migration]:-0}"
+  expected="$(trim $(drush state-get epa.allowed_unprocessed.$migration))"
+  expected="${$expected:-0}"
 
   # If the unprocessed count is above the threshold, return a failure.
   if test "$remaining" -gt "$expected"; then
@@ -226,14 +238,6 @@ latest_revision_migrations=(
 
 path_redirect_migrations=(
   upgrade_d7_path_redirect
-)
-
-# Collection of allowed thresholds for unprocessed items in a migration. The array is
-# keyed by migration name, and the values are the expected unprocessed count. If 0
-# entities are expected to remain unprocessed, then it is safe to omit that migration from
-# this array. See the run_migration function for how this is used.
-declare -A expected_unprocessed=(
-  # (syntax note: the square brackets are required)
 )
 
 # Import taxonomy terms, groups, and users.
