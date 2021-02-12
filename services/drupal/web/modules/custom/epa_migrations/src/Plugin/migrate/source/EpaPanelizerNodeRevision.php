faf15353f8bf3fcd5793d7a050cbb51640902c54
Brian Tofte-Schumacher <btofte-schumacher@forumone.com>
2021-02-12 11:12:04 -0800

    [EPAD8] Update panelizer source queries

--- services/drupal/web/modules/custom/epa_migrations/src/Plugin/migrate/source/EpaPanelizerNodeRevision.php
+++ services/drupal/web/modules/custom/epa_migrations/src/Plugin/migrate/source/EpaPanelizerNodeRevision.php
@@ -27,20 +27,27 @@ class EpaPanelizerNodeRevision extends NodeRevision {
     // Get the default Node query.
     $query = parent::query();
 
-    // For all node types, exclude nodes that use these layouts:
-    // * onecol_page
-    // * six_pack
-    // Exclude nodes that use the twocol_page layout for all node types except
-    // web_area.
-    $query->innerJoin('panelizer_entity', 'pe', 'n.vid = pe.revision_id AND pe.entity_id = n.nid AND pe.entity_type = :type', [':type' => 'node']);
-    $query->innerJoin('panels_display', 'pd', 'pe.did = pd.did');
-    $query->condition('pe.did', 0, '<>');
-    $query->condition('pd.layout', 'onecol_page', '<>');
-    $query->condition('pd.layout', 'six_pack', '<>');
+    $query->leftJoin('panelizer_entity', 'pe', 'nr.vid = pe.revision_id AND pe.entity_id = n.nid AND pe.entity_type = :type', [':type' => 'node']);
+    $query->leftJoin('panels_display', 'pd', 'pe.did = pd.did');
+    $query->innerJoin('node_revision_epa_states', 'nres', 'nres.vid = nr.vid');
 
-    if ($this->configuration['node_type'] !== 'web_area') {
-      $query->condition('pd.layout', 'twocol_page', '<>');
-    }
+    $query->addField('nres','state');
+    $query->addField('pe', 'did');
+    $query->addField('pd', 'layout');
+
+    // Only include records where one of the following is true:
+    // * Does not use panelizer twocol_page layout unless it is a web area node
+    // * Does not use panelizer onecol_page or six_pack layout.
+
+    $and = $query->andConditionGroup()
+      ->condition('pd.layout', 'twocol_page')
+      ->condition('n.type', 'web_area');
+
+    $or = $query->orConditionGroup()
+      ->condition($and)
+      ->condition('pd.layout', ['onecol_page', 'twocol_page', 'six_pack'], 'NOT IN');
+
+    $query->condition($or);
 
     return $query;
   }
