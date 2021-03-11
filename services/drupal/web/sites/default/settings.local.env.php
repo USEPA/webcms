007cdeeb5845388fdd16b831089d9c015cc55216
Aaron Zinck <azinck@gmail.com>
2021-03-11 18:27:23 -0500

    EPAD8-1189 update S3 module to fix problem serving image derivatives

--- services/drupal/web/sites/default/settings.local.env.php
+++ services/drupal/web/sites/default/settings.local.env.php
@@ -18,11 +18,12 @@ $settings['s3fs.secret_key'] = 'minio_secret';
 
 // Override the endpoint used by s3fs in order to talk to Minio.
 $config['s3fs.settings']['use_customhost'] = TRUE;
+$config['s3fs.settings']['use_path_style_endpoint'] = TRUE;
 $config['s3fs.settings']['hostname'] = 'minio:9000';
 
-// Override output to point to localhost:8888/drupal in order to see Minio-saved files
+// Override output to point to localhost:8888 in order to see Minio-saved files
 $config['s3fs.settings']['use_cname'] = TRUE;
-$config['s3fs.settings']['domain'] = 'localhost:8888/drupal';
+$config['s3fs.settings']['domain'] = 'localhost:8888';
 
 // Map twig cache onto shared filesystem to allow drush to clear and write twig cache for local development.
 $settings['php_storage']['twig']['directory'] = '/var/www/html/web/sites/default/files/tmp/cache/twig';
