3d06c1606362ce675d6db84b6f5c9b4169bc6562
msglover <glover.michael@epa.gov>
2021-03-15 12:11:54 -0400

    Create backend_override.tf

---
+++ terraform/infrastructure/backend_override.tf
@@ -0,0 +1,6 @@
+terraform {
+  # Backend configuration is left unspecified here: there should be an external backend
+  # configuration file used on each `terraform init`
+  # cf. https://www.terraform.io/docs/backends/config.html#partial-configuration
+  backend "http" {}
+}
