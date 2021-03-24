db96afbc348d059808c4a5cf56e48aa2006caeea
Brian Tofte-Schumacher <btofte-schumacher@forumone.com>
2021-03-24 11:22:05 -0700

    [EPAD8] Turn off auto_entitylabel during migration

--- services/drupal/web/sites/default/settings.php
+++ services/drupal/web/sites/default/settings.php
@@ -851,6 +851,7 @@ switch ($env_state) {
   case 'migration':
     $settings['redirect.settings']['auto_redirect'] = false;
     $config['purge.plugins']['queuers'] = [['plugin_id'=> 'coretags', 'status' => false]];
+    $config['auto_entitylabel.settings.node.faq']['status'] = 0;
   case 'run':
     $settings['memcache']['servers'] = [getenv('WEBCMS_CACHE_HOST') .':11211' => 'default'];
     $settings['memcache']['options'] = [
