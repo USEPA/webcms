d16085481c437b1777cab2498338159d77cc7019
gustavderdrache <aford@forumone.com>
2021-06-29 11:38:59 -0400

    EPAD8-1175: Log more in entrypoint

--- services/drupal/scripts/ecs/drupal-entrypoint.sh
+++ services/drupal/scripts/ecs/drupal-entrypoint.sh
@@ -10,9 +10,13 @@ if test -n "${WEBCMS_NEW_RELIC_LICENSE:-}" && test "${WEBCMS_NEW_RELIC_LICENSE}"
     -e "s/REPLACE_WITH_REAL_KEY/${WEBCMS_NEW_RELIC_LICENSE}/" \
     -e "s/newrelic.appname[[:space:]]=[[:space:]].*/newrelic.appname=\"${WEBCMS_NEW_RELIC_APPNAME}\"" \
     "$newrelic_ini_path"
+
+  status=enabled
 else
   # Otherwise, disable the extension.
   echo 'newrelic.enabled=false' >> "$newrelic_ini_path"
+
+  status=disabled
 fi
 
 {
@@ -21,6 +25,8 @@ fi
   echo 'newrelic.daemon.app_connect_timeout=15s'
   echo 'newrelic.daemon.start_timeout=5s'
 } >> "$newrelic_ini_path"
+
+echo "Status of New Relic in $newrelic_ini_path: $status"
 
 # Forward to the original image entrypoint
 exec docker-php-entrypoint "$@"
