dc7f38fa9c5b87d4fc089c83ccf555000332a5f1
Brian Tofte-Schumacher <btofte-schumacher@forumone.com>
2021-07-21 13:29:13 -0700

    [EPAD8-496] Ensure both profiles populate a link when the form is saved

--- services/drupal/web/modules/custom/epa_wysiwyg/js/epa_wysiwyg_linkit.js
+++ services/drupal/web/modules/custom/epa_wysiwyg/js/epa_wysiwyg_linkit.js
@@ -8,10 +8,22 @@
   Drupal.behaviors.epaWysiwygLinkit = {
     attach: function (context) {
       $webAreaInput = $('[name="attributes[href_web_area_content]"]');
-      $defaultInput = $('[name="attributes[href]"]');
+      $defaultInput = $('[name="attributes[href_default]"]');
+      $hrefInput = $('[name="attributes[href]"]');
+
+      // Populate the defaultInput with the value of hrefInput on load, if there
+      // is one.
+      $('[name="attributes[href]"]', context).once('epaWysiwygLinkitInitInput').each(function() {
+        if ($hrefInput.val() != '') {
+          $defaultInput.val($hrefInput.val());
+          $hrefInput.val('');
+        }
+      });
+
+
       // Clear the linkit input values when the profile is changed to reset the
       // autocomplete results.
-      $('[name="attributes[select-profile]"]', context).once('epaWysiwygLinkit').change(function() {
+      $('[name="attributes[select-profile]"]', context).once('epaWysiwygLinkitResetInput').change(function() {
         $defaultInput.val('');
         $webAreaInput.val('');
       });
