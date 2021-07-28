f9de5bae05ae8b9d6cd8d695b03a72314bfc0ad8
Aaron Zinck <azinck@gmail.com>
2021-07-28 12:47:43 -0400

    Ensure we are using GET requests for our alerts to benefit from caching

--- services/drupal/web/modules/custom/epa_alerts/src/Plugin/Block/PublicAlertsBlock.php
+++ services/drupal/web/modules/custom/epa_alerts/src/Plugin/Block/PublicAlertsBlock.php
@@ -23,6 +23,11 @@ class PublicAlertsBlock extends BlockBase {
 
     $build['#theme'][] = 'epa_alerts';
     $build['#alertContext'] = 'public';
+
+    // Must manually add this since we're not directly rendering the view so the
+    // views_ajax_get module doesn't get an opportunity to attach it dynamically.
+    $build['#attached']['drupalSettings']['viewsAjaxGet']['public_alerts'] = 'public_alerts';
+
     $build['#attached']['library'][] = 'epa_alerts/epaAlerts';
     $build['#attached']['drupalSettings']['epaAlerts']['context'] = 'public';
 
