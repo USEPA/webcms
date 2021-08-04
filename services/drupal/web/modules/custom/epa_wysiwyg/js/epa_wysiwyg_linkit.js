edc62ea55c201193dcd2a6d9f4fb590847ae1853
Brian Tofte-Schumacher <btofte-schumacher@forumone.com>
2021-08-03 18:01:19 -0700

    [EPAD8-467] Declare constants

--- services/drupal/web/modules/custom/epa_wysiwyg/js/epa_wysiwyg_linkit.js
+++ services/drupal/web/modules/custom/epa_wysiwyg/js/epa_wysiwyg_linkit.js
@@ -7,9 +7,9 @@
 
   Drupal.behaviors.epaWysiwygLinkit = {
     attach: function (context) {
-      $webAreaInput = $('[name="attributes[href_web_area_content]"]');
-      $defaultInput = $('[name="attributes[href_default]"]');
-      $hrefInput = $('[name="attributes[href]"]');
+      const $webAreaInput = $('[name="attributes[href_web_area_content]"]');
+      const $defaultInput = $('[name="attributes[href_default]"]');
+      const $hrefInput = $('[name="attributes[href]"]');
 
       // Populate the defaultInput with the value of hrefInput on load, if there
       // is one.
