9bba82ec41d77ea2d487aac9607b66fcb814f7df
Josh Wagner <jwagner@forumone.com>
2021-09-22 16:05:57 -0400

    [EPAD8-1301] add check for meta language field changes

--- services/drupal/web/themes/epa_theme/js/src/external-links.es6.js
+++ services/drupal/web/themes/epa_theme/js/src/external-links.es6.js
@@ -3,13 +3,33 @@ import Drupal from 'drupal';
 
 (function(Drupal) {
   Drupal.behaviors.externalLinks = {
-    attach(context) {
+    attach(context, settings) {
       const externalLinks = context.querySelectorAll(
         "a:not([href=''], [href*='.gov'], [href*='.mil'], [href^='#'], [href^='?'], [href^='/'], [href^='.'], [href^='javascript:'], [href^='mailto:'], [href^='tel:'], [href*='webcms-uploads-dev.s3.amazonaws.com'], [href*='webcms-uploads-stage.s3.amazonaws.com'], [href*='webcms-uploads-prod.s3.amazonaws.com'])"
       );
+      const translate = {
+        en: 'Exit',
+        es: 'Nuevo',
+        ar: 'جديد',
+        zh_CN: '新',
+        zh_TW: '新',
+        fr: 'Nouveau',
+        ht: 'Nouvo',
+        it: ' Nuovo',
+        ko: '신규',
+        pt: 'Novo',
+        ru: 'Новый',
+        tl: 'Bago',
+        vi: 'Mới',
+      };
       externalLinks.forEach(function(el) {
         if (el.hasAttribute('href')) {
-          const translated = Drupal.t('Exit');
+          let translated = Drupal.t('Exit');
+          const article = el.closest('article[lang]');
+          if (article) {
+            const lang = article.getAttribute('lang');
+            translated = translate[lang];
+          }
           el.insertAdjacentHTML(
             'beforeend',
             `<span class="usa-tag external-link__tag">${translated}</span>`
