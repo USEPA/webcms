c7047356422a46515b76684a55ed896340e480f4
Carson Oldson <coldson@forumone.com>
2021-10-08 11:28:29 -0500

    [EPAD8-1442] Removing post update hook as it's logic has now been consolidated in the previous hook. We are now solely looking for any nodes that have a missing scheduled transiton and re-saving them.

--- services/drupal/web/modules/custom/epa_workflow/epa_workflow.post_update.php
+++ services/drupal/web/modules/custom/epa_workflow/epa_workflow.post_update.php
@@ -165,75 +165,15 @@ function epa_workflow_post_update_0003_fix_nodes_missing_review_deadline(&$sandb
 }
 
 /**
- * Find all nodes with a review deadline coming up in the next 3 weeks (or in the past)
- * and that don't have a scheduled transition set and re-save them.
+ * Find all nodes missing a scheduled transition and re-save them.
  */
-function epa_workflow_post_update_0004_fix_nodes_missing_transition_date_three_weeks(&$sandbox) {
-  // Create dates 3 weeks in the future from now. We'll use this to find any
-  // nodes that have a deadline coming up in the next 3 weeks (or in the past)
-  $now = \Drupal\Core\Datetime\DrupalDateTime::createFromTimestamp(time());
-  $now->add(new DateInterval('P3W'));
-  $formatted_three_weeks = $now->format('Y-m-d\TH:i:s');
+function epa_workflow_post_update_0004_fix_nodes_missing_transition_date(&$sandbox) {
 
   if (!isset($sandbox['total'])) {
     // Query all published nodes that don't have a scheduled transition date, but
     // do have a review deadline
     $result = \Drupal::entityQuery('node')
-      ->condition('status', 1)
-      ->condition('field_review_deadline', $formatted_three_weeks, '<=')
-      ->notExists('field_scheduled_transition')
-      ->execute();
-
-    $sandbox['total'] = count($result);
-    $sandbox['current'] = 0;
-  }
-
-  $batch_size = 25;
-
-  $nids = \Drupal::entityQuery('node')
-    ->condition('status', 1)
-    ->condition('field_review_deadline', $formatted_three_weeks, '<=')
-    ->notExists('field_scheduled_transition')
-    ->range(0, $batch_size)
-    ->execute();
-
-  if (empty($nids)) {
-    $sandbox['#finished'] = 1;
-    return;
-  }
-
-  /** @var \Drupal\node\NodeInterface[] $nodes */
-  $nodes = \Drupal::entityTypeManager()
-    ->getStorage('node')
-    ->loadMultiple($nids);
-
-  foreach ($nodes as $node) {
-    // Re-saving nodes that have a missing transition scheduled will trigger
-    // the EPAPublished service to set the review deadline based on the Type
-    // term the node is set to.
-    $node->save();
-    $sandbox['current']++;
-  }
-
-  Drupal::logger('epa_workflow')->notice($sandbox['current'] . ' nodes with missing scheduled transition processed.');
-
-  if ($sandbox['current'] >= $sandbox['total']) {
-    $sandbox['#finished'] = 1;
-  } else {
-    $sandbox['#finished'] = ($sandbox['current'] / $sandbox['total']);
-  }
-}
-
-
-/**
- * Find all nodes missing a scheduled transition and re-saves them.
- */
-function epa_workflow_post_update_0005_fix_nodes_missing_scheduled_transition(&$sandbox) {
-  if (!isset($sandbox['total'])) {
-    // Query all published nodes that don't have a scheduled transition date, but
-    // do have a review deadline
-    $result = \Drupal::entityQuery('node')
-      ->condition('status', 1)
+      - >condition('status', 1)
       ->notExists('field_scheduled_transition')
       ->execute();
 
