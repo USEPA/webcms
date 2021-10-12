e5e338b9cc73949a999ebf6d193adabe8df70b38
Aaron Zinck <azinck@gmail.com>
2021-10-12 17:04:20 -0400

    Switch to deploying using drush deploy command so that we can run post-config-import scripts

--- .buildkite/run-drush.sh
+++ .buildkite/run-drush.sh
@@ -13,8 +13,7 @@ set -euo pipefail
 # shellcheck disable=SC2016
 script='drush --debug --uri="$WEBCMS_SITE_URL" sset system.maintenance_mode 1 --input-format=integer
 drush --debug --uri="$WEBCMS_SITE_URL" cr
-drush --debug --uri="$WEBCMS_SITE_URL" updb -y
-drush --debug --uri="$WEBCMS_SITE_URL" cim -y
+drush --debug --uri="$WEBCMS_SITE_URL" deploy -y
 drush --debug --uri="$WEBCMS_SITE_URL" sset system.maintenance_mode 0 --input-format=integer
 drush --debug --uri="$WEBCMS_SITE_URL" cr'
 
