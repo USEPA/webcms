ad79f1b05bb25083076ccb2689afd7721d6bb811
Dan Mouyard <dcmouyard@gmail.com>
2021-10-27 13:21:44 -0400

    Revert "Merge branch 'EPAD8-1444-uswds-library' into integration"
    This reverts commit 8ae5a6152b6a98542583de8114594c592725692f, reversing
    changes made to 4b61ff4feae8a22bb2c11e5c0d0e0dde6000de65.

--- services/drupal/web/themes/epa_theme/js/src/scripts.es6.js
+++ services/drupal/web/themes/epa_theme/js/src/scripts.es6.js
@@ -1,14 +1,38 @@
 // Custom scripts file
-
-// Include USWDS Javascript.
-require('uswds');
-
+// Include the USWDS Accordion script.
+// This makes the component available globally. If you're only using it on certain pages,
+// include it on a template-specific script file instead.
+// Be sure to initialize any components as well (see init() function below.)
 import domready from 'domready';
+// import accordion from 'uswds/src/js/components/accordion.js';
 import navigation from './modules/navigation';
+// import banner from 'uswds/src/js/components/banner.js';
+// DEPRECATED table sort method.
 import tablesort from './modules/tablesort';
+// USWDS JS sortable tables. Replaces tablesort going forward.
+// import table from 'uswds/src/js/components/table';
 import backToTop from './modules/_back-to-top.es6';
 import setScrollbarProperty from './modules/scrollbar-property.es6';
 import 'svgxuse';
+import {
+  accordion,
+  banner,
+  characterCount,
+  comboBox,
+  datePicker,
+  dateRangePicker,
+  fileInput,
+  footer,
+  inputPrefixSuffix,
+  modal,
+  password,
+  search,
+  skipnav,
+  table,
+  timePicker,
+  tooltip,
+  validator,
+} from 'uswds/src/js/components/index';
 
 (function() {
   'use strict';
@@ -32,9 +56,26 @@ import 'svgxuse';
 
   domready(() => {
     document.documentElement.classList.remove('no-js');
-    navigation();
+    accordion.on(document.body);
+    banner.on(document.body);
+    navigation(); // If used with the USWDS accordion component, the navigation must run after it.
     tablesort();
+    table.on(document.body);
     backToTop();
     setScrollbarProperty();
+    characterCount.on(document.body);
+    comboBox.on(document.body);
+    datePicker.on(document.body);
+    dateRangePicker.on(document.body);
+    fileInput.on(document.body);
+    footer.on(document.body);
+    inputPrefixSuffix.on(document.body);
+    modal.on(document.body);
+    password.on(document.body);
+    search.on(document.body);
+    skipnav.on(document.body);
+    timePicker.on(document.body);
+    tooltip.on(document.body);
+    validator.on(document.body);
   });
 })();
