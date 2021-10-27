9ef6e4b965a2c3f2ad61521dc516c3b9d47a5456
Dan Mouyard <dcmouyard@gmail.com>
2021-10-27 13:22:05 -0400

    Revert "Merge branch 'EPAD8-1444-uswds-library' into integration"
    This reverts commit d2120c8d38fa1e9b960119f08a8941d4507edfbd, reversing
    changes made to 3871419d4e187bd8cd401228da54b2d274aad5e9.

--- services/drupal/web/themes/epa_theme/js/src/scripts.es6.js
+++ services/drupal/web/themes/epa_theme/js/src/scripts.es6.js
@@ -4,35 +4,16 @@
 // include it on a template-specific script file instead.
 // Be sure to initialize any components as well (see init() function below.)
 import domready from 'domready';
-// import accordion from 'uswds/src/js/components/accordion.js';
+import accordion from 'uswds/src/js/components/accordion.js';
 import navigation from './modules/navigation';
-// import banner from 'uswds/src/js/components/banner.js';
+import banner from 'uswds/src/js/components/banner.js';
 // DEPRECATED table sort method.
 import tablesort from './modules/tablesort';
 // USWDS JS sortable tables. Replaces tablesort going forward.
-// import table from 'uswds/src/js/components/table';
+import table from 'uswds/src/js/components/table';
 import backToTop from './modules/_back-to-top.es6';
 import setScrollbarProperty from './modules/scrollbar-property.es6';
 import 'svgxuse';
-import {
-  accordion,
-  banner,
-  characterCount,
-  comboBox,
-  datePicker,
-  dateRangePicker,
-  fileInput,
-  footer,
-  inputPrefixSuffix,
-  modal,
-  password,
-  search,
-  skipnav,
-  table,
-  timePicker,
-  tooltip,
-  validator,
-} from 'uswds/src/js/components/index';
 
 (function() {
   'use strict';
@@ -63,19 +44,5 @@ import {
     table.on(document.body);
     backToTop();
     setScrollbarProperty();
-    characterCount.on(document.body);
-    comboBox.on(document.body);
-    datePicker.on(document.body);
-    dateRangePicker.on(document.body);
-    fileInput.on(document.body);
-    footer.on(document.body);
-    inputPrefixSuffix.on(document.body);
-    modal.on(document.body);
-    password.on(document.body);
-    search.on(document.body);
-    skipnav.on(document.body);
-    timePicker.on(document.body);
-    tooltip.on(document.body);
-    validator.on(document.body);
   });
 })();
