3f87d994d9a756eb0c48a907dc2f6af27e80e431
Carson Oldson <coldson@forumone.com>
2021-11-22 13:49:15 -0600

    [EPAD8-1302] Updating filter to use dependency injection.

--- services/drupal/web/modules/custom/epa_web_areas/src/Plugin/views/filter/UsersGroupsFilter.php
+++ services/drupal/web/modules/custom/epa_web_areas/src/Plugin/views/filter/UsersGroupsFilter.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\epa_web_areas\Plugin\views\filter;
 
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\group\GroupMembershipLoaderInterface;
 use Drupal\views\Plugin\views\display\DisplayPluginBase;
-use Drupal\views\Plugin\views\filter\ManyToOne;
+use Drupal\views\Plugin\views\filter\InOperator;
 use Drupal\views\ViewExecutable;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Filters nodes based on user's groups
@@ -13,8 +16,47 @@ use Drupal\views\ViewExecutable;
  *
  * @ViewsFilter("epa_web_areas_users_groups")
  */
-class UsersGroupsFilter extends ManyToOne {
+class UsersGroupsFilter extends InOperator implements ContainerFactoryPluginInterface {
 
+  /**
+   * Group membership loader service.
+   *
+   * @var \Drupal\group\GroupMembershipLoaderInterface
+   */
+  protected $groupMembershipLoader;
+
+  /**
+   * UsersGroupsFilter constructor.
+   *
+   * @param array $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, GroupMembershipLoaderInterface $group_membership_loader) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->groupMembershipLoader = $group_membership_loader;
+  }
+
+  /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   *
+   * @return \Drupal\epa_web_areas\Plugin\views\filter\UsersGroupsFilter|\Drupal\views\Plugin\views\PluginBase
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('group.membership_loader')
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
     parent::init($view, $display, $options);
     $this->valueTitle = $this->t('Filter on Groups the current user belongs to.');
@@ -22,27 +64,40 @@ class UsersGroupsFilter extends ManyToOne {
     $this->currentDisplay = $view->current_display;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   public function query() {
     if (!empty($this->value)) {
       $groups = $this->getAllGroupsByUser();
       if (!empty($groups)) {
-        // @TODO: Why do I have to use the alias field? Do I need to add a relationship in hook_views_data_alter()?
-        $this->query->addWhere('AND', 'group_content_field_data_node_field_data.gid', $groups, 'IN');
+        $this->ensureMyTable();
+        $this->query->addWhere('AND', $this->tableAlias . '.' . $this->realField, $groups, 'IN');
       }
     }
   }
 
-  public function generateOptions() {
+  /**
+   * Helper to generate options for our filter.
+   *
+   * @return array
+   *   Array of options.
+   */
+  public function generateOptions(): array {
     return [
-      $this->t('In My Groups'),
+      $this->t('In my Web Areas'),
     ];
   }
 
-  private function getAllGroupsByUser() {
+  /**
+   * Gets all Group entity IDs for the currently logged in user.
+   *
+   * @return array
+   *   Group IDs the current user is a member of.
+   */
+  private function getAllGroupsByUser(): array {
     $groups = [];
-    /** @var \Drupal\group\GroupMembershipLoaderInterface $grp_membership_service */
-    $grp_membership_service = \Drupal::service('group.membership_loader');
-    $memberships = $grp_membership_service->loadByUser();
+    $memberships = $this->groupMembershipLoader->loadByUser();
     foreach ($memberships as $group) {
       $groups[] = $group->getGroup()->id();
     }
