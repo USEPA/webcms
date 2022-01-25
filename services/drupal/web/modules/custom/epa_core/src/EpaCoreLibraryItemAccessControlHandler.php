5177109a424afbc9a2f5acf989dbd7b5890e8c5f
Nathan Han <nh0713@uw.edu>
2022-01-24 22:44:44 -0800

    [EPAD8-1595] Add logic to override access handler

---
+++ services/drupal/web/modules/custom/epa_core/src/EpaCoreLibraryItemAccessControlHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\epa_core;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+use Drupal\paragraphs_library\LibraryItemAccessControlHandler;
+
+/**
+ * Access controller for the paragraphs entity.
+ *
+ * @see \Drupal\paragraphs\Entity\Paragraph.
+ */
+class EpaCoreLibraryItemAccessControlHandler extends LibraryItemAccessControlHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $library_item, $operation, AccountInterface $account) {
+    // In case a library item is unpublished, only allow access if a user has
+    // administrative permission. Ensure to collect the required cacheability
+    // metadata and combine both the published and the referenced access check
+    // together, both must allow access if unpublished.
+    $access = AccessResult::allowed()->addCacheableDependency($library_item);
+    if ($operation === 'view' && !$library_item->isPublished()) {
+      $access = $access->andIf(AccessResult::allowedIfHasPermission($account, $this->entityType->getAdminPermission()));
+    }
+
+    $roles = $account->getRoles();
+
+    // Users can update if they have 'edit paragraph library item' permission
+    // and they own the paragraph; or if they have admin permission.
+    if ($operation === 'update') {
+      $access = $access->allowedIf($library_item->getOwnerId() == $account->id());
+      $access = $access->andIf(AccessResult::allowedIfHasPermission($account, 'edit paragraph library item'));
+      $access = $access->orIf(AccessResult::allowedIfHasPermission($account, $this->entityType->getAdminPermission()));
+    }
+
+    // Only users with system_webmaster role or admin permission can delete library items.
+    if ($operation === 'delete') {
+      $access = $access->allowedIf(in_array('system_webmaster', $roles));
+      $access = $access->orIf(AccessResult::allowedIfHasPermission($account, $this->entityType->getAdminPermission()));
+    }
+    return $access;
+  }
+
+}
