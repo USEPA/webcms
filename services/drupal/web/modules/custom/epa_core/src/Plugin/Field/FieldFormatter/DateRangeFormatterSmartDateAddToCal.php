a854f2e5f730343f3054e8c1023ad026062e432c
Ryan Jensen <rjensen@rjensen-forumone.local>
2022-02-11 16:31:43 -0500

    EPAD8-1596: Adding a stopping point

--- services/drupal/web/modules/custom/epa_core/src/Plugin/Field/FieldFormatter/DateRangeFormatterSmartDateAddToCal.php
+++ services/drupal/web/modules/custom/epa_core/src/Plugin/Field/FieldFormatter/DateRangeFormatterSmartDateAddToCal.php
@@ -14,13 +14,13 @@ use Drupal\epa_core\Plugin\Field\FieldFormatter\AddToCalendarFormatter;
 *
 * @FieldFormatter(
 *   id = "date_range_without_time_smartdate",
-*   label = @Translation("Date range Smart Date (without time)"),
+*   label = @Translation("Date range Smart Date includes AddtoCal"),
 *   field_types = {
 *     "smartdate"
 *   }
 * )
 */
-class DateRangeFormatterRangeFormatterSmartDate extends AddToCalendarFormatter {
+class DateRangeFormatterSmartDateAddToCal extends AddToCalendarFormatter {
 
   /**
    * {@inheritdoc}
@@ -37,11 +37,12 @@ class DateRangeFormatterRangeFormatterSmartDate extends AddToCalendarFormatter {
     foreach ($items as $delta => $item) {
 
       if (!empty($item->value) && !empty($item->end_value)) {
-        /** @var \Drupal\Core\Datetime\DrupalDateTime $start_date */
         $start_date = $item->value;
-        /** @var \Drupal\Core\Datetime\DrupalDateTime $end_date */
-        // $item->timezone;
         $end_date = $item->end_value;
+
+        // If the default timezone is used please populate the timezone to site default
+        $timezone = (!empty($item->timezone) ? $item->timezone : 'America/New_York');
+
         if ($start_date !== $end_date) {
           $format = $this->getSetting('several_years');
           if (date('Y', $start_date) === date('Y', $end_date)) {
@@ -54,42 +55,43 @@ class DateRangeFormatterRangeFormatterSmartDate extends AddToCalendarFormatter {
             $format = $this->getSetting('one_day');
           }
 
-          $date_str = \Drupal::service('date.formatter')->format($start_date, 'custom', preg_replace('/\{([a-zA-Z])\}/', '{\\\$1}', t($format)), $item->timezone);
+          $date_str = \Drupal::service('date.formatter')->format($start_date, 'custom', preg_replace('/\{([a-zA-Z])\}/', '{\\\$1}', t($format)), $timezone);
           $matches = array();
           if (preg_match_all('/\{([a-zA-Z])\}/', $date_str, $matches)) {
             foreach ($matches[1] as $match) {
-              $date_str = preg_replace('/\{' . $match . '\}/', \Drupal::service('date.formatter')->format($end_date, 'custom', $match, $item->timezone), $date_str);
+              $date_str = preg_replace('/\{' . $match . '\}/', \Drupal::service('date.formatter')->format($end_date, 'custom', $match, $timezone), $date_str);
             }
           }
           $elements[$delta] = ['#markup' => '<span class="date-display-range">' . $date_str . '</span>',];
 
         }
         else {
-          $elements[$delta] = ['#markup' => \Drupal::service('date.formatter')->format($start_date, 'custom', t($this->getSetting('one_day')), $item->timezone)];
+          $elements[$delta] = ['#markup' => \Drupal::service('date.formatter')->format($start_date, 'custom', t($this->getSetting('one_day')), $timezone)];
         }
       }
 
+      $t = date_default_timezone_get();
       // Adding AddtoCal
       $form = new AddToCalForm($entity, $settings, $delta);
       $form = \Drupal::formBuilder()->getForm($form);
 
-      /** @var DateTimeFieldItemList $dates */
-      $dates = $entity->get($field_name);
-      // Date range field has different structure
+      // Adjusting the timezone
+      $now = new \DateTime('', new \DateTimeZone($timezone));
+      $now = $now->format('Y-m-d H:i:s');
 
-      if (!empty($start_date) && !empty($end_date)) {
-        $start_date_object = $dates[$delta];
-      }
-      else {
-        $start_date_object = $dates[$delta]->date;
-      }
-      //$d = new \DateTime('now');
-      //$u =  strtotime($start_date, 'z');
-      //$form['#access'] = strtotime($cur_date) < strtotime($start_date);
-      $now = strtotime('now');
-      $t = $start_date;
-      $form['#access'] = true;
-      $test =  $now < $start_date;
+      $event_start_date = new \DateTime(date('Y-m-d H:i:s', $start_date), new \DateTimeZone(($timezone)));
+      $event_start_date = $event_start_date->format('Y-m-d H:i:s');
+
+      // Need to compare the current time & the event date.
+      // To display the addtocal or not.
+      // @TODO: need to work on getting timezones accounted for
+      // When the event was setup for 3pm ASKT
+      // $now = current time 11am
+      // $event_start_date = 19.
+      // So it looks like while comparing them may be a little more difficult since the $now time (- offset) and
+      // $event-start_date appears to be (+ offset) to eastern.
+      $form['#access'] =  $now < $event_start_date;
+
       $elements[$delta]['addtocal'] = $form;
     }
     return $elements;
