62122f160e493d316e21d0c8b88257dd6f9dc849
Josh Wagner <jwagner@forumone.com>
2022-07-13 11:43:13 -0400

    [EPAD8-1770] add whitelist for intranet links

---
+++ services/drupal/web/themes/epa_theme/js/src/protected-links.es6.js
@@ -0,0 +1,46 @@
+// Eternal Links script
+import Drupal from 'drupal';
+
+(function(Drupal) {
+  Drupal.behaviors.protectedLinks = {
+    attach(context, settings) {
+      const allowedDomains = [
+        'work.epa.gov',
+        'intranet.epa.gov',
+        'usepa.sharepoint.com',
+      ];
+      function linkIsProtected(linkElement) {
+        let external = false;
+
+        allowedDomains.forEach(domain => {
+          if (
+            linkElement.host === domain ||
+            linkElement.host.endsWith(`.${domain}`)
+          ) {
+            external = true;
+          }
+        });
+
+        return external;
+      }
+      const externalLinks = context.querySelectorAll(
+        "a:not([href=''], [href^='#'], [href^='?'], [href^='/'], [href^='.'], [href^='javascript:'], [href^='mailto:'], [href^='tel:'])"
+      );
+
+      externalLinks.forEach(function(el) {
+        if (el.hasAttribute('href') && linkIsProtected(el)) {
+          const translatedAccessible = Drupal.t('Link to protected area');
+          el.insertAdjacentHTML(
+            'beforeend',
+            `
+<span class="protected-link__tag" title="${translatedAccessible}">
+  <svg class="usa-icon" aria-hidden="true" focusable="false" role="img">
+    <use href="/themes/epa_theme/images/sprite.svg#lock"></use>
+  </svg>
+</span>`
+          );
+        }
+      });
+    },
+  };
+})(Drupal);
