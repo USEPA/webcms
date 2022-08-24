64fb24884587f568c77f0186373aeba07a4c292d
Josh Wagner <jwagner@forumone.com>
2022-08-24 13:40:15 -0400

    [EPAD8-1545] update to different method, remove two-up-element

--- services/drupal/web/themes/epa_theme/js/src/before-after-swipe.es6.js
+++ services/drupal/web/themes/epa_theme/js/src/before-after-swipe.es6.js
@@ -1,11 +1,25 @@
 // Eternal Links script
 import Drupal from 'drupal';
-import twoUp from 'two-up-element';
 
 (function(Drupal) {
   Drupal.behaviors.beforeAfterSwipe = {
-    attach() {
-      twoUp();
+    attach(context, settings) {
+      const beforeAfter = context.querySelector('.before-after-swipe');
+      const slider = beforeAfter.querySelector('.before-after-swipe__slider');
+
+      function update(e) {
+        const sliderPos = e.target.value;
+        e.target.parentElement.style.setProperty(
+          '--split-point',
+          `${sliderPos}%`
+        );
+        context.querySelector(
+          '.before-after-swipe__slider-button'
+        ).style.left = `calc(${sliderPos}%`;
+      }
+
+      slider.addEventListener('input', update);
+      slider.addEventListener('change', update);
     },
   };
 })(Drupal);
