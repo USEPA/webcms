491dff13441a482fd541cc57325d48605ceb0e04
gustavderdrache <aford@forumone.com>
2022-11-16 12:52:10 -0500

    EPAD8-1900: Consume basic auth credentials

--- terraform/webcms/shared.tf
+++ terraform/webcms/shared.tf
@@ -158,6 +158,10 @@ data "aws_ssm_parameter" "newrelic_license" {
   name = "/webcms/${var.environment}/${var.site}/${var.lang}/secrets/newrelic-license"
 }
 
+data "aws_ssm_parameter" "basic_auth" {
+  name = "/webcms/${var.environment}/${var.site}/${var.lang}/secrets/basic-authn"
+}
+
 data "aws_ssm_parameter" "hash_salt" {
   name = "/webcms/${var.environment}/${var.site}/${var.lang}/secrets/drupal-hash-salt"
 }
@@ -186,6 +190,7 @@ locals {
     { name = "WEBCMS_MAIL_PASS", valueFrom = data.aws_ssm_parameter.mail_pass.value },
     { name = "WEBCMS_SAML_SP_KEY", valueFrom = data.aws_ssm_parameter.saml_sp_key.value },
     { name = "WEBCMS_NEW_RELIC_LICENSE", valueFrom = data.aws_ssm_parameter.newrelic_license.value },
+    { name = "WEBCMS_BASIC_AUTH", valueFrom = data.aws_ssm_parameter.basic_auth.value },
   ]
 
   #endregion
