2b5467a623da1cdbe4962b56271a346808a1a279
Carson Oldson <coldson@forumone.com>
2023-01-10 15:46:46 -0600

    [EPAD8-1931] Adding sparkles.svg. Updating EPA New ckeditor plugin to use new icon. Adding 'New!' text be displayed along with the icon.

--- services/drupal/web/modules/custom/epa_wysiwyg/js/ckeditor5_plugins/epaNew/src/epa-new-ui.js
+++ services/drupal/web/modules/custom/epa_wysiwyg/js/ckeditor5_plugins/epaNew/src/epa-new-ui.js
@@ -1,7 +1,7 @@
 import { Plugin } from 'ckeditor5/src/core';
 import { ButtonView } from 'ckeditor5/src/ui';
 // @TODO: Get .gif or .png files working in webpack to then change the icon out.
-import icon from '../../../../icons/simpleBox.svg';
+import icon from '../../../../icons/sparkles.svg';
 
 export default class EPANewUi extends Plugin {
   init() {
@@ -14,9 +14,10 @@ export default class EPANewUi extends Plugin {
 
       // Create the toolbar button.
       buttonView.set({
-        label: editor.t('New! Icon'),
+        label: editor.t('New!'),
         icon,
         tooltip: true,
+        withText: true,
       });
 
       // Bind the state of the button to the command.
