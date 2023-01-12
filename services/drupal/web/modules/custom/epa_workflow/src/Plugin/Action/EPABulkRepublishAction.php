eaac5b6199898d3790134e85cb2c10da7c0ce08c
Aaron Zinck <azinck@gmail.com>
2023-01-12 17:00:24 -0500

    EPAD8-1942 simplify code for republishing the current revision

--- services/drupal/web/modules/custom/epa_workflow/src/Plugin/Action/EPABulkRepublishAction.php
+++ services/drupal/web/modules/custom/epa_workflow/src/Plugin/Action/EPABulkRepublishAction.php
@@ -35,7 +35,7 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
  *
  * @Action(
  *   id = "epa_workflow_bulk_republish",
- *   label = @Translation("Bulk Republish"),
+ *   label = @Translation("Republish content"),
  *   type = ""
  * )
  */
@@ -94,30 +94,24 @@ class EPABulkRepublishAction extends ViewsBulkOperationsActionBase implements Co
    */
   public function execute($entity = NULL)
   {
-    $valid_states = ['published_expiring', 'published_day_til_expire', 'published_needs_review'];
-    $content_moderation_state = ContentModerationState::loadFromModeratedEntity($entity)->get('moderation_state')->getString();
-    // Return default revision, which will be published or archived.
-    // Including in check for 'archived' in the case we ever add this state to the content view, this action will still work as expected.
-    if (!$entity->isDefaultRevision() || $content_moderation_state == 'archived') {
-      $entity_vids = \Drupal::entityTypeManager()->getStorage('node')->revisionIds($entity);
-      foreach ($entity_vids as $vid) {
-        if (\Drupal::entityTypeManager()->getStorage('node')->loadRevision($vid)->isPublished()) {
-          $entity = \Drupal::entityTypeManager()->getStorage('node')->loadRevision($vid);
-          $content_moderation_state = ContentModerationState::loadFromModeratedEntity($entity)->get('moderation_state')->getString();
-        }
-      }
+    // Ensure we're acting on the default revision
+    if (!$entity->isDefaultRevision()) {
+      $entity = \Drupal::entityTypeManager()->getStorage($entity->getEntityTypeId())->load($entity->id());
     }
-    if (in_array($content_moderation_state, $valid_states) && $entity->isPublished()) {
-      $new_state = 'published';
-      $entity->set('moderation_state', $new_state);
+
+    // We only want to allow re-publishing when the default revision is published but not actually in the "published" state.
+    // i.e., we should be able to republish items that are in "Published, Needs Review"
+    if ($entity->isPublished() && $entity->moderation_state->value !== 'published') {
+      $entity->set('moderation_state', 'published');
       if ($entity instanceof RevisionLogInterface) {
-        $entity->setRevisionLogMessage('Changed moderation state to Published.');
+        $entity->setRevisionLogMessage('Bulk re-publishing.');
         $entity->setRevisionUserId($this->account->id());
       }
       $entity->save();
+      return $this->t('Content has been re-published.');
     }
 
-    return $this->t('The selected content with current revisions matching any of these states: "Published needs review", Published scheduled for expiration", and "Published, one day until expiration" were republished and state set to "Published".');
+    return $this->t('Only Published content that has entered the review cycle can be re-published..');
   }
 
   /**
