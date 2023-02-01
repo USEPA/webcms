d266bca472f7ba8d58f6255ae1321de1b0ec3fea
Aimee Rydarowski <arydarowski@Aimees-MacBook-Pro.local>
2023-02-01 11:09:44 -0500

    EPAD8-1747 updates to accommodate Public Notice comments due date

--- services/drupal/web/modules/custom/epa_workflow/src/EPAPublished.php
+++ services/drupal/web/modules/custom/epa_workflow/src/EPAPublished.php
@@ -22,25 +22,6 @@ class EPAPublished extends EPAModeration {
   public function process(ContentModerationStateInterface $moderation_entity) {
     parent::process($moderation_entity);
 
-    $this->setReviewDeadline();
-    $this->setLastPublishedDate();
-
-    if ($this->contentEntityRevision->hasField('field_review_deadline') &&
-      $this->contentEntityRevision->hasField('field_expiration_date') &&
-      !$this->contentEntityRevision->get('field_expiration_date')->isEmpty() &&
-      $this->contentEntityRevision->field_review_deadline->value > $this->contentEntityRevision->field_expiration_date->value) {
-      $this->scheduleTransition('field_expiration_date', 'unpublished', TRUE);
-    }
-
-    if ($this->contentHasFieldValue('field_review_deadline')) {
-      $transition_date = $this->contentEntityRevision->field_review_deadline->date;
-      $transition_date->sub(new \DateInterval("P3W"));
-      $this->scheduleTransition($transition_date, 'published_needs_review');
-    }
-    else {
-      $this->logger->warning('A review transition for %title could not be set because no review deadline is available.', ['%title' => $this->contentEntityRevision->label()]);
-    }
-
     // We need to record when a Web Area Homepage node is first published.
     if ($this->contentEntityRevision->bundle() == 'web_area' && $this->contentEntityRevision->getEntityTypeId() == 'node') {
       $groups = \Drupal::service('epa_web_areas.web_areas_helper')->getNodeReferencingGroups($this->contentEntityRevision);
@@ -112,6 +93,25 @@ class EPAPublished extends EPAModeration {
           $node->field_type = $term->id();
         }
       }
+    }
+
+    $this->setReviewDeadline();
+    $this->setLastPublishedDate();
+
+    if ($this->contentEntityRevision->hasField('field_review_deadline') &&
+      $this->contentEntityRevision->hasField('field_expiration_date') &&
+      !$this->contentEntityRevision->get('field_expiration_date')->isEmpty() &&
+      $this->contentEntityRevision->field_review_deadline->value > $this->contentEntityRevision->field_expiration_date->value) {
+      $this->scheduleTransition('field_expiration_date', 'unpublished', TRUE);
+    }
+
+    if ($this->contentHasFieldValue('field_review_deadline')) {
+      $transition_date = $this->contentEntityRevision->field_review_deadline->date;
+      $transition_date->sub(new \DateInterval("P3W"));
+      $this->scheduleTransition($transition_date, 'published_needs_review');
+    }
+    else {
+      $this->logger->warning('A review transition for %title could not be set because no review deadline is available.', ['%title' => $this->contentEntityRevision->label()]);
     }
   }
 
