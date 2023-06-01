9101e64c9596d1ae499d9368f9c3e9edc160dfd5
Carson Oldson <coldson@forumone.com>
2023-06-01 14:50:43 -0500

    [EPAD8-2153] Updating 'Edit Media' and 'Copy file path' plugin icons.

--- services/drupal/web/modules/custom/epa_wysiwyg/js/ckeditor5_plugins/epaEditMedia/src/epadrupalmediaui.js
+++ services/drupal/web/modules/custom/epa_wysiwyg/js/ckeditor5_plugins/epaEditMedia/src/epadrupalmediaui.js
@@ -1,7 +1,6 @@
 import { Plugin } from 'ckeditor5/src/core';
 import { ButtonView } from 'ckeditor5/src/ui';
-// @TODO: Get .gif or .png files working in webpack to then change the icon out.
-import linkIcon from "../../../../../../../core/modules/ckeditor5/icons/code-block.svg"
+import linkIcon from "../../../../icons/file-pen-solid.svg";
 import EditDrupalMediaCommand from "./editdrupalmediacommand";
 
 export default class EpaDrupalMediaUI extends Plugin {
