36854049a60bac2dab7aa44dae66880e2a749dce
Dan Mouyard <dcmouyard@gmail.com>
2023-09-14 16:02:49 -0400

    EPAD8-2161: Tweak vid-copy script and markup

--- services/drupal/web/themes/epa_theme/js/src/vid-copy.es6.js
+++ services/drupal/web/themes/epa_theme/js/src/vid-copy.es6.js
@@ -4,16 +4,17 @@ import Drupal from 'drupal';
 (function(Drupal) {
   Drupal.behaviors.vidCopy = {
     attach(context) {
-      const vidCopyButton = context.querySelector('.field--vid-button');
       const windowHost = window.location.hostname;
       const vidCopyURL = context
         .querySelector('.revision-link')
         .getAttribute('href');
 
-      vidCopyButton.addEventListener('click', function(e) {
-        e.preventDefault();
-        const vidWriteText = `https://${windowHost}${vidCopyURL}`;
-        navigator.clipboard.writeText(vidWriteText);
+      once('vid-copy', '.js-vid-copy', context).forEach(button => {
+        button.addEventListener('click', function(e) {
+          e.preventDefault();
+          const vidWriteText = `https://${windowHost}${vidCopyURL}`;
+          navigator.clipboard.writeText(vidWriteText);
+        });
       });
     },
   };
