8a5975e46112d50e47eacc4a9ea4805457104b40
gustavderdrache <aford@forumone.com>
2023-10-04 10:19:31 -0400

    EPAD8-2200: Remove Traefik network resources

--- terraform/network/parameters.tf
+++ terraform/network/parameters.tf
@@ -87,14 +87,6 @@ resource "aws_ssm_parameter" "drupal_security_group" {
   tags = var.tags
 }
 
-resource "aws_ssm_parameter" "traefik_security_group" {
-  name  = "/webcms/${var.environment}/security-groups/traefik"
-  type  = "String"
-  value = aws_security_group.traefik.id
-
-  tags = var.tags
-}
-
 resource "aws_ssm_parameter" "alb_security_group" {
   name  = "/webcms/${var.environment}/security-groups/alb"
   type  = "String"
