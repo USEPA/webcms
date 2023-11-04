aaff18584d0ad95dd45cc1371aae82ad2cc8392a
Carson Oldson <coldson@forumone.com>
2023-11-04 13:51:03 -0500

    [EPAD8-2270] Optimizing and adding some additional logic to ensure paths are correctly checked.

--- services/drupal/web/modules/custom/epa_web_areas/epa_web_areas.post_update.php
+++ services/drupal/web/modules/custom/epa_web_areas/epa_web_areas.post_update.php
@@ -12,6 +12,9 @@ function epa_web_areas_post_update_0001_web_area_menu_disable_home_link(&$sandbo
     ->getStorage('group')
     ->loadMultiple();
 
+  /** @var \Drupal\path_alias\AliasManager $path_manager */
+  $path_manager = \Drupal::service('path_alias.manager');
+
   /** @var \Drupal\menu_link_content\MenuLinkContentStorageInterface $menu_link_storage */
   $menu_link_storage = \Drupal::entityTypeManager()
     ->getStorage('menu_link_content');
@@ -27,15 +30,6 @@ function epa_web_areas_post_update_0001_web_area_menu_disable_home_link(&$sandbo
   ];
 
   foreach ($groups as $group) {
-    \Drupal::logger('epa_web_areas_menu_cleanup')->notice("Processing group: " . $group->label() . " ID: " . $group->id());
-    // Get the field_homepage node from the group to use for comparing urls.
-    $homepage = $group->get('field_homepage')->entity;
-    if (!$homepage) {
-      \Drupal::logger('epa_web_areas_menu_cleanup')->notice("Group does not have homepage set: " . $group->label() . " ID: " . $group->id());
-      continue;
-    }
-    $homepage_url = $homepage->toUrl()->toString();
-
     // Now load any group menus based on that group. Should only be one, however
     // there's no reason they can't have more than one.
     $content_menus = group_content_menu_get_menus_per_group($group);
@@ -46,7 +40,7 @@ function epa_web_areas_post_update_0001_web_area_menu_disable_home_link(&$sandbo
       $tree = $menu_tree->load($menu_name, $menu_parameters);
 
       if (empty($tree)) {
-        \Drupal::logger('epa_web_areas_menu_cleanup')->notice("Group does not have any menu items: " . $group->label() . " ID: " . $group->id());
+        \Drupal::logger('epa_web_areas_menu_cleanup')->notice("{$group->label()} ({$group->id()}) menu does not have any menu items.");
         continue;
       }
 
@@ -57,24 +51,63 @@ function epa_web_areas_post_update_0001_web_area_menu_disable_home_link(&$sandbo
       if ($first = reset($tree)) {
         // If it has children log and continue on. That will manually need to be cleaned up.
         if ($first->hasChildren) {
-          \Drupal::logger('epa_web_areas_menu_cleanup')->notice("Group menu first link has children: " . $group->label() . " ID: " . $group->id());
+          \Drupal::logger('epa_web_areas_menu_cleanup')->notice("{$group->label()} ({$group->id()}) menu first link has children. Skipping.");
           continue;
         }
+
         /** @var \Drupal\menu_link_content\Plugin\Menu\MenuLinkContent $first_link */
         $first_link = $first->link;
-        $first_link_url = $first_link->getUrlObject()->toString();
+        /** @var \Drupal\Core\Url $first_link_url */
+        $first_link_url = $first_link->getUrlObject();
+        // The menu link could be external. If so, we need to get the actual path
+        if ($first_link_url->isExternal()) {
+          $parts = parse_url($first_link_url->toString());
+          if (str_contains($parts['host'], 'epa.gov') && isset($parts['path'])) {
+            // If it's an EPA link, we can just use the path.
+            $first_link_path = $parts['path'];
+          }
+          else {
+            \Drupal::logger('epa_web_areas_menu_cleanup')->notice("{$group->label()} ({$group->id()}) first menu link URL goes off domain");
+            continue;
+          }
+        }
+        else {
+          $first_link_path = $first_link_url->toString();
+        }
+
+        // Get the field_homepage node from the group to use for comparing urls.
+        $homepage = $group->get('field_homepage')->entity;
+        if (!$homepage) {
+          \Drupal::logger('epa_web_areas_menu_cleanup')->notice("{$group->label()} ({$group->id()}) does not have homepage set");
+          continue;
+        }
+        if (!$homepage->isPublished()) {
+          \Drupal::logger('epa_web_areas_menu_cleanup')->notice("{$group->label()} ({$group->id()}) homepage is not published");
+          continue;
+        }
+
+        $homepage_url_path = $homepage->toUrl()->toString();
+
+        if (str_starts_with($homepage_url_path, '/node/')) {
+          $homepage_url_path = $path_manager->getAliasByPath($homepage_url_path);
+        }
+
+        if (str_starts_with($first_link_path, '/node/')) {
+          $first_link_path = $path_manager->getAliasByPath($first_link_path);
+        }
+
         // Compare if the group's field_homepage URL is going to the same place as
         // the first menu link AND if it doesn't have any children.
-        if ($first_link_url == $homepage_url) {
+        if ($first_link_path == $homepage_url_path) {
           // First link is the homepage link AND it has no children. Now load the
           // actual menu link entity and set it to be disabled.
           $plugin_definition = $first_link->getPluginDefinition();
           $actual_menu_link = $menu_link_storage->load($plugin_definition['metadata']['entity_id']);
           $actual_menu_link->set('enabled', FALSE)->save();
-          \Drupal::logger('epa_web_areas_menu_cleanup')->notice("Successfully disabled home link for: " . $group->label() . " ID: " . $group->id());
+          \Drupal::logger('epa_web_areas_menu_cleanup')->notice("Successfully disabled home link for {$group->label()} ({$group->id()}");
         }
         else {
-          \Drupal::logger('epa_web_areas_menu_cleanup')->notice("Group menu first link does not go to homepage: " . $group->label() . " ID: " . $group->id());
+          \Drupal::logger('epa_web_areas_menu_cleanup')->notice("{$group->label()} ({$group->id()}) menu first link does not go to homepage: \n Links compared were: $first_link_path and $homepage_url_path");
         }
       }
     }
