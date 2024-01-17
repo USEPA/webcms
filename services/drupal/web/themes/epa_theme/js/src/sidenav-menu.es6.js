563d1763997769614b69b576820ce01690f9aef3
Sarah Proper <sarah@sarahproper.com>
2024-01-17 00:49:00 -0700

    EPAD8-2328 focus in sidenav, but only when last item is visible

--- services/drupal/web/themes/epa_theme/js/src/sidenav-menu.es6.js
+++ services/drupal/web/themes/epa_theme/js/src/sidenav-menu.es6.js
@@ -8,6 +8,14 @@ import Drupal from 'drupal';
       const sideNavMenu = context.querySelector('.menu--sidenav-nav');
       const sideNavOverlay = context.querySelector('.menu-sidenav__overlay');
       const sideNavTrigger = context.getElementById('web-area-menu__button');
+      const blockWebArea = context.getElementById('block-webareamenu');
+
+      const focusable = Array.from(
+        blockWebArea.querySelectorAll('button, [href], input, select, textarea')
+      ).filter(item => item.tabIndex !== -1 && item.hidden !== true);
+      const numberFocusElements = focusable.length;
+      const firstFocusableElement = focusable[0];
+      const lastFocusableElement = focusable[numberFocusElements - 1];
 
       function toggleVisiblity() {
         pageBody.classList.toggle('js-menu-sidenav--active');
@@ -29,6 +37,24 @@ import Drupal from 'drupal';
         const subBtnSib = subNav.previousElementSibling;
         subNav.setAttribute('id', subId);
         subBtnSib.setAttribute('aria-controls', subId);
+      });
+
+      blockWebArea.addEventListener('keydown', event => {
+        if (event.key === 'Tab') {
+          if (
+            event.shiftKey &&
+            document.activeElement === firstFocusableElement
+          ) {
+            event.preventDefault();
+            lastFocusableElement.focus();
+          } else if (
+            document.activeElement === lastFocusableElement &&
+            !event.shiftKey
+          ) {
+            event.preventDefault();
+            firstFocusableElement.focus();
+          }
+        }
       });
     },
   };
