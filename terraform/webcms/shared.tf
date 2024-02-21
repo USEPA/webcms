159ee77228c0b974669f5e0134fb84573f9050b2
gustavderdrache <aford@forumone.com>
2024-02-21 16:45:21 -0500

    EPAD8-2384: Remove CloudWatch agent

--- terraform/webcms/shared.tf
+++ terraform/webcms/shared.tf
@@ -94,10 +94,6 @@ data "aws_ssm_parameter" "ecr_metrics" {
   name = "/webcms/${var.environment}/${var.site}/${var.lang}/ecr/metrics"
 }
 
-data "aws_ssm_parameter" "ecr_cloudwatch" {
-  name = "/webcms/${var.environment}/${var.site}/${var.lang}/ecr/cloudwatch"
-}
-
 #endregion
 
 #region Log groups
@@ -112,10 +108,6 @@ data "aws_ssm_parameter" "nginx_log_group" {
 
 data "aws_ssm_parameter" "drush_log_group" {
   name = "/webcms/${var.environment}/${var.site}/${var.lang}/log-groups/drush"
-}
-
-data "aws_ssm_parameter" "agent_log_group" {
-  name = "/webcms/${var.environment}/${var.site}/${var.lang}/log-groups/cloudwatch-agent"
 }
 
 data "aws_ssm_parameter" "fpm_metrics_log_group" {
