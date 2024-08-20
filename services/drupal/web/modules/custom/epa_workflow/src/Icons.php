f2c699ed036c05bc72d79dbb3eed3173461cbcac
Dan Mouyard <dcmouyard@gmail.com>
2024-08-20 13:29:58 -0400

    EPAD8-2387: Move Drupal UI icons to separate sprite

--- services/drupal/web/modules/custom/epa_workflow/src/Icons.php
+++ services/drupal/web/modules/custom/epa_workflow/src/Icons.php
@@ -3,10 +3,10 @@
 namespace Drupal\epa_workflow;
 
 abstract class Icons {
-  const CLOSE = 'drupal-ex';
-  const CYCLE = 'drupal-cycle';
-  const GRID = 'drupal-grid';
-  const LIVE = 'drupal-live';
-  const PENCIL = 'drupal-pencil';
-  const SHOW = 'drupal-show';
+  const CLOSE = 'ex';
+  const CYCLE = 'cycle';
+  const GRID = 'grid';
+  const LIVE = 'live';
+  const PENCIL = 'pencil';
+  const SHOW = 'show';
 }
